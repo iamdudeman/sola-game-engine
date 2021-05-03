22a5f066f2b13a61584443cb9feaecbf6b4ec46d
iamdudeman <timothy.solum@gmail.com>
2021-05-02 19:39:37 -0500

    AbstractSola used for Swing too.

--- examples/swing/src/main/java/technology/sola/engine/examples/swing/Main.java
+++ examples/swing/src/main/java/technology/sola/engine/examples/swing/Main.java
@@ -1,47 +1,103 @@
 package technology.sola.engine.examples.swing;
 
 import technology.sola.engine.assets.AssetLoader;
+import technology.sola.engine.ecs.AbstractEcsSystem;
+import technology.sola.engine.ecs.Component;
+import technology.sola.engine.ecs.World;
 import technology.sola.engine.graphics.Color;
 import technology.sola.engine.graphics.RenderMode;
 import technology.sola.engine.graphics.Renderer;
 import technology.sola.engine.graphics.SolaImage;
 import technology.sola.engine.platform.swing.SolaImageAssetMapper;
-import technology.sola.engine.platform.swing.SwingContainer;
+import technology.sola.engine.platform.swing.SolaSwing;
 
 public class Main {
   public static void main(String[] args) {
-    SwingContainer swingContainer = new SwingContainer("Swing Test" ,800, 600);
+    ExampleGame exampleGame = new ExampleGame("Swing Test", 800, 600, 30);
 
-    Renderer renderer = swingContainer.getRenderer();
+    exampleGame.show();
+  }
 
-    renderer.clear();
-    renderer.setPixel(5, 5, Color.WHITE);
-    renderer.setPixel(6, 5, Color.BLUE);
-    renderer.setPixel(6, 6, Color.RED);
-    renderer.setPixel(6, 7, Color.GREEN);
+  private static class ExampleGame extends SolaSwing {
+    private SolaImage solaImage;
 
-    renderer.drawLine(20, 50, 20, 100, Color.WHITE);
-    renderer.drawLine(50, 20, 100, 20, Color.WHITE);
+    public ExampleGame(String title, int width, int height, int updatesPerSecond) {
+      super(title, width, height, updatesPerSecond);
+    }
 
-    renderer.fillRect(100, 100, 60, 80, Color.GREEN);
-    renderer.drawRect(100, 100, 60, 80, Color.RED);
+    @Override
+    protected void onInit() {
+      AssetLoader assetLoader = new AssetLoader();
+      assetLoader.addAssetMapper(new SolaImageAssetMapper());
+      assetLoader.addAsset("test_tiles", "test_tiles.png");
 
-    renderer.drawRect(300, 150, 5, 5, Color.GREEN);
-    renderer.fillCircle(300, 150, 100.5f, Color.BLUE);
-    renderer.drawCircle(300, 150, 100.5f, Color.RED);
+      solaImage = assetLoader.getAsset("test_tiles", SolaImage.class);
 
-    AssetLoader assetLoader = new AssetLoader();
-    assetLoader.addAssetMapper(new SolaImageAssetMapper());
-    assetLoader.addAsset("test_tiles", "test_tiles.png");
-    SolaImage solaImage = assetLoader.getAsset("test_tiles", SolaImage.class);
-    renderer.drawImage(400, 400, solaImage);
-    renderer.drawImage(400, 530, solaImage.getSubImage(1, 1, 16, 16));
+      World world = new World(1);
 
-    renderer.fillRect(180, 530, 50, 50, new Color(255, 0, 0, 255));
-    renderer.setRenderMode(RenderMode.ALPHA);
-    renderer.fillRect(210, 530, 50, 50, new Color(150, 255, 0, 0));
-    renderer.setRenderMode(RenderMode.NORMAL);
+      world.createEntity().addComponent(new Position());
 
-    swingContainer.show();
+      ecsSystemContainer.setWorld(world);
+      ecsSystemContainer.add(new TestSystem());
+    }
+
+    @Override
+    protected void onRender(Renderer renderer) {
+      renderer.clear();
+      renderer.setPixel(5, 5, Color.WHITE);
+      renderer.setPixel(6, 5, Color.BLUE);
+      renderer.setPixel(6, 6, Color.RED);
+      renderer.setPixel(6, 7, Color.GREEN);
+
+      renderer.drawLine(20, 50, 20, 100, Color.WHITE);
+      renderer.drawLine(50, 20, 100, 20, Color.WHITE);
+
+      renderer.fillRect(100, 100, 60, 80, Color.GREEN);
+      renderer.drawRect(100, 100, 60, 80, Color.RED);
+
+      renderer.drawRect(300, 150, 5, 5, Color.GREEN);
+      renderer.fillCircle(300, 150, 100.5f, Color.BLUE);
+      renderer.drawCircle(300, 150, 100.5f, Color.RED);
+
+      renderer.drawImage(400, 400, solaImage);
+      renderer.drawImage(400, 530, solaImage.getSubImage(1, 1, 16, 16));
+
+      renderer.fillRect(180, 530, 50, 50, new Color(255, 0, 0, 255));
+      renderer.setRenderMode(RenderMode.ALPHA);
+      renderer.fillRect(210, 530, 50, 50, new Color(150, 255, 0, 0));
+      renderer.setRenderMode(RenderMode.NORMAL);
+
+      ecsSystemContainer.getWorld().getEntitiesWithComponents(Position.class)
+        .forEach(entity -> {
+          Position position = entity.getComponent(Position.class);
+
+          renderer.fillRect(position.x, position.y, 50, 50, Color.RED);
+        });
+
+      super.onRender(renderer);
+    }
+  }
+
+
+  private static class Position implements Component {
+    private int x = 50;
+    private int y = 50;
+  }
+
+  private static class TestSystem extends AbstractEcsSystem {
+    @Override
+    public void update(World world, float deltaTime) {
+      world.getEntitiesWithComponents(Position.class)
+        .forEach(entity -> {
+          Position position = entity.getComponent(Position.class);
+
+          position.x++;
+        });
+    }
+
+    @Override
+    public int getOrder() {
+      return 0;
+    }
   }
 }
