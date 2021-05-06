734d04d5a25fd4e9c84ba14f048aad168b1aeab2
iamdudeman <timothy.solum@gmail.com>
2021-05-05 22:58:34 -0500

    Updated NoSolaPlatform to ensure it is never used for launching an AbstractSola

--- engine/src/main/java/technology/sola/engine/core/NoSolaPlatform.java
+++ engine/src/main/java/technology/sola/engine/core/NoSolaPlatform.java
@@ -6,16 +6,21 @@ import technology.sola.engine.graphics.Renderer;
 class NoSolaPlatform extends AbstractSolaPlatform {
   @Override
   protected void init(AssetLoader assetLoader) {
-
+    // Not needed
   }
 
   @Override
   protected void start() {
-
+    // Not needed
   }
 
   @Override
   protected void render(Renderer renderer) {
+    // Not needed
+  }
 
+  @Override
+  public void launch(AbstractSola abstractSola) {
+    throw new IllegalStateException("NoSolaPlatform should never be used to Launch an AbstractSola instance");
   }
 }
