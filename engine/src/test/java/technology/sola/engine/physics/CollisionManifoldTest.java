4e769c1dac4b69f53d25d521ba47b39505975755
Tim Solum <timothy.solum@gmail.com>
2021-05-17 13:59:45 -0500

    Added CollisionManifoldTest

---
+++ engine/src/test/java/technology/sola/engine/physics/CollisionManifoldTest.java
@@ -0,0 +1,41 @@
+package technology.sola.engine.physics;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import technology.sola.engine.ecs.Entity;
+import technology.sola.math.linear.Vector2D;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class CollisionManifoldTest {
+  @Test
+  void whenSameEntities_withDifferentOrder_shouldBeEqual() {
+    Entity mockEntityA = Mockito.mock(Entity.class);
+    Entity mockEntityB = Mockito.mock(Entity.class);
+    Mockito.when(mockEntityA.getId()).thenReturn(1);
+    Mockito.when(mockEntityB.getId()).thenReturn(2);
+
+    CollisionManifold first = new CollisionManifold(mockEntityA, mockEntityB, new Vector2D(0, 0), 0);
+    CollisionManifold second = new CollisionManifold(mockEntityB, mockEntityA, new Vector2D(5, 5), 5);
+
+    assertEquals(first, second);
+    assertEquals(first.hashCode(), second.hashCode());
+  }
+
+  @Test
+  void whenDifferentEntities_shouldNotBeEqual() {
+    Entity mockEntityA = Mockito.mock(Entity.class);
+    Entity mockEntityB = Mockito.mock(Entity.class);
+    Entity mockEntityC = Mockito.mock(Entity.class);
+    Mockito.when(mockEntityA.getId()).thenReturn(1);
+    Mockito.when(mockEntityB.getId()).thenReturn(2);
+    Mockito.when(mockEntityC.getId()).thenReturn(3);
+
+    CollisionManifold first = new CollisionManifold(mockEntityA, mockEntityB, new Vector2D(0, 0), 0);
+    CollisionManifold second = new CollisionManifold(mockEntityB, mockEntityC, new Vector2D(0, 0), 0);
+
+    assertNotEquals(first, second);
+    assertNotEquals(first.hashCode(), second.hashCode());
+  }
+}
