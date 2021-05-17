eb03a6a7d8343750ac372456227138624adf24cb
Tim Solum <timothy.solum@gmail.com>
2021-05-17 12:03:48 -0500

    Added several classes for physics system logic

---
+++ engine/src/main/java/technology/sola/engine/physics/CollisionUtils.java
@@ -0,0 +1,169 @@
+package technology.sola.engine.physics;
+
+import technology.sola.engine.ecs.Entity;
+import technology.sola.engine.physics.component.ColliderComponent;
+import technology.sola.engine.physics.component.PositionComponent;
+import technology.sola.math.SolKanaMath;
+import technology.sola.math.geometry.Circle;
+import technology.sola.math.geometry.Rectangle;
+import technology.sola.math.linear.Vector2D;
+
+final class CollisionUtils {
+  static CollisionManifold calculateCollisionManifold(
+    Entity entityA, Entity entityB,
+    PositionComponent positionA, PositionComponent positionB,
+    ColliderComponent colliderA, ColliderComponent colliderB
+  ) {
+    switch (colliderA.getColliderType()) {
+      case AABB:
+        switch (colliderB.getColliderType()) {
+          case AABB:
+            Rectangle rectangleA = colliderA.asRectangle(positionA);
+            Rectangle rectangleB = colliderB.asRectangle(positionB);
+
+            return calculateAABBVsAABB(entityA, entityB, rectangleA, rectangleB);
+          case CIRCLE:
+            Rectangle rectangle = colliderA.asRectangle(positionA);
+            Circle circle = colliderB.asCircle(positionB);
+
+            return calculateAABBVsCircle(entityA, entityB, rectangle, circle);
+          default:
+        }
+        break;
+      case CIRCLE:
+        switch (colliderB.getColliderType()) {
+          case AABB:
+            Rectangle rectangle = colliderB.asRectangle(positionB);
+            Circle circle = colliderA.asCircle(positionA);
+
+            return calculateAABBVsCircle(entityB, entityA, rectangle, circle);
+          case CIRCLE:
+            Circle circleA = colliderA.asCircle(positionA);
+            Circle circleB = colliderB.asCircle(positionB);
+
+            return calculateCircleVsCircle(entityA, entityB, circleA, circleB);
+          default:
+        }
+      default:
+    }
+
+    return null;
+  }
+
+  private static CollisionManifold calculateAABBVsCircle(
+    Entity rectEntity, Entity circeEntity,
+    Rectangle rectangle, Circle circle
+  ) {
+    Vector2D circleCenter = circle.getCenter();
+    Vector2D closestPointOnRectangle = SolKanaMath.clamp(rectangle.getMin(), rectangle.getMax(), circleCenter);
+    boolean isCircleCenterInsideRectangle = circleCenter.equals(closestPointOnRectangle);
+
+    if (isCircleCenterInsideRectangle) {
+      // Find the closest edge since that is the closest point on rectangle for the normal
+      float minDistanceX = circleCenter.x - rectangle.getMin().x;
+      float maxDistanceX = rectangle.getMax().x - circleCenter.x;
+      float minDistanceY = circleCenter.y - rectangle.getMin().y;
+      float maxDistanceY = rectangle.getMax().y - circleCenter.y;
+
+      if (maxDistanceY < minDistanceY && maxDistanceY < maxDistanceX && maxDistanceY < minDistanceX) {
+        closestPointOnRectangle = new Vector2D(circleCenter.x, rectangle.getMax().y);
+      } else if (minDistanceY < maxDistanceX && minDistanceY < minDistanceX) {
+        closestPointOnRectangle = new Vector2D(circleCenter.x, rectangle.getMin().y);
+      } else if (maxDistanceX < minDistanceX) {
+        closestPointOnRectangle = new Vector2D(rectangle.getMax().x, circleCenter.y);
+      } else {
+        closestPointOnRectangle = new Vector2D(rectangle.getMin().x, circleCenter.y);
+      }
+    }
+
+    // Normal
+    Vector2D diff = closestPointOnRectangle.subtract(circleCenter);
+
+    if (diff.magnitudeSq() > circle.getRadius() * circle.getRadius()) {
+      return null;
+    }
+
+    float penetration = circle.getRadius() - closestPointOnRectangle.distance(circleCenter);
+    Vector2D normal = diff.normalize();
+
+    // If not inside
+    if (!isCircleCenterInsideRectangle) {
+      normal = normal.scalar(-1);
+    }
+
+    return new CollisionManifold(rectEntity, circeEntity, normal, penetration);
+  }
+
+  private static CollisionManifold calculateAABBVsAABB(
+    Entity entityA, Entity entityB,
+    Rectangle rectangleA, Rectangle rectangleB
+  ) {
+    Vector2D aBoxMin = rectangleA.getMin();
+    Vector2D aBoxMax = rectangleA.getMax();
+    Vector2D bBoxMin = rectangleB.getMin();
+    Vector2D bBoxMax = rectangleB.getMax();
+
+    Vector2D posDiffMin = bBoxMin.subtract(aBoxMin);
+    Vector2D posDiffMax = bBoxMax.subtract(aBoxMax);
+
+    float aWidth = aBoxMax.x - aBoxMin.x;
+    float bWidth = bBoxMax.x - bBoxMin.x;
+    float xAxisOverlap = (aWidth + bWidth - (Math.abs(posDiffMin.x) + Math.abs(posDiffMax.x))) / 2;
+
+    // No collision if no x overlap
+    if (xAxisOverlap <= 0) {
+      return null;
+    }
+
+    float aHeight = aBoxMax.y - aBoxMin.y;
+    float bHeight = bBoxMax.y - bBoxMin.y;
+    float yAxisOverlap = (aHeight + bHeight - (Math.abs(posDiffMin.y) + Math.abs(posDiffMax.y))) / 2;
+
+    // No collision if no y overlap
+    if (yAxisOverlap <= 0) {
+      return null;
+    }
+
+    // Collision found, calculate normal and penetration
+    final Vector2D normal;
+    final float penetration;
+
+    if (xAxisOverlap < yAxisOverlap) {
+      penetration = xAxisOverlap;
+      if (posDiffMin.x > 0)
+        normal = new Vector2D(1, 0);
+      else
+        normal = new Vector2D(-1, 0);
+    } else {
+      penetration = yAxisOverlap;
+      if (posDiffMin.y > 0)
+        normal = new Vector2D(0, 1);
+      else
+        normal = new Vector2D(0, -1);
+    }
+
+    return new CollisionManifold(entityA, entityB, normal, penetration);
+  }
+
+  private static CollisionManifold calculateCircleVsCircle(
+    Entity entityA, Entity entityB,
+    Circle circleA, Circle circleB
+  ) {
+    Vector2D posA = circleA.getCenter();
+    Vector2D posB = circleB.getCenter();
+    float distance = posA.distance(posB);
+    float penetration = circleA.getRadius() + circleB.getRadius() - distance;
+
+    if (penetration <= 0) {
+      return null;
+    }
+
+    Vector2D normal = posB.subtract(posA).normalize();
+
+    return new CollisionManifold(entityA, entityB, normal, penetration);
+  }
+
+  private CollisionUtils() {
+  }
+}
+
---
+++ engine/src/test/java/technology/sola/engine/physics/SpacialHashMapTest.java
@@ -0,0 +1,155 @@
+package technology.sola.engine.physics;
+
+import org.junit.jupiter.api.Test;
+import technology.sola.engine.ecs.Entity;
+import technology.sola.engine.ecs.World;
+import technology.sola.engine.physics.component.ColliderComponent;
+import technology.sola.engine.physics.component.PositionComponent;
+import technology.sola.math.linear.Vector2D;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SpacialHashMapTest {
+  @Test
+  void whenNoEntitiesWithCollider_withCellSizeSpecified_shouldNotThrowException() {
+    assertDoesNotThrow(() -> new SpacialHashMap(Collections.emptyList(), 500));
+  }
+
+  @Test
+  void whenEntitiesHaveNoColliderComponent_shouldNotThrowException() {
+    World world = new World(2);
+    world.createEntity().addComponent(new PositionComponent());
+    world.createEntity().addComponent(new PositionComponent());
+
+    assertDoesNotThrow(() -> new SpacialHashMap(world.getEntitiesWithComponents()));
+  }
+
+  @Test
+  void whenEntitiesHaveNoPositionComponent_shouldNotThrowException() {
+    World world = new World(2);
+    world.createEntity().addComponent(ColliderComponent.rectangle(10, 10));
+    world.createEntity().addComponent(ColliderComponent.rectangle(10, 10));
+
+    assertDoesNotThrow(() -> new SpacialHashMap(world.getEntitiesWithComponents()));
+  }
+
+  @Test
+  void whenCreatedWithCellSize_shouldUsePassedValue() {
+    World world = createTestWorld();
+    SpacialHashMap spacialHashMap = new SpacialHashMap(world.getEntitiesWithComponents(), 45);
+
+    assertEquals(45, spacialHashMap.cellSize);
+  }
+
+  @Test
+  void whenCreatedWithCellSize_shouldNowAllowSmallerThanLargestEntity() {
+    World world = createTestWorld();
+
+    assertThrows(IllegalArgumentException.class, () -> new SpacialHashMap(world.getEntitiesWithComponents(), 19));
+  }
+
+  @Test
+  void whenCreatedWithoutCellSize_shouldUseLargestEntityWidthOrHeight() {
+    World world = createTestWorld();
+    SpacialHashMap spacialHashMap = new SpacialHashMap(world.getEntitiesWithComponents());
+
+    assertEquals(40, spacialHashMap.cellSize);
+  }
+
+  @Test
+  void whenEntitiesRegistered_shouldProperlyPlaceInBuckets() {
+    World world = createTestWorld();
+    List<Entity> entities = world.getEntitiesWithComponents();
+    SpacialHashMap spacialHashMap = new SpacialHashMap(entities);
+
+    List<Vector2D> points = spacialHashMap.getBucketIdsForEntity(entities.get(0));
+    assertEquals(new Vector2D(0, 0), points.get(0));
+    assertEquals(new Vector2D(0, 0), points.get(1));
+    assertEquals(new Vector2D(0, 0), points.get(2));
+    assertEquals(new Vector2D(0, 0), points.get(3));
+
+    points = spacialHashMap.getBucketIdsForEntity(entities.get(1));
+    assertEquals(new Vector2D(0, 0), points.get(0));
+    assertEquals(new Vector2D(1, 0), points.get(1));
+    assertEquals(new Vector2D(0, 1), points.get(2));
+    assertEquals(new Vector2D(1, 1), points.get(3));
+
+    points = spacialHashMap.getBucketIdsForEntity(entities.get(2));
+    assertEquals(new Vector2D(1, 1), points.get(0));
+    assertEquals(new Vector2D(1, 1), points.get(1));
+    assertEquals(new Vector2D(1, 1), points.get(2));
+    assertEquals(new Vector2D(1, 1), points.get(3));
+
+    points = spacialHashMap.getBucketIdsForEntity(entities.get(3));
+    assertEquals(new Vector2D(0, 0), points.get(0));
+    assertEquals(new Vector2D(0, 0), points.get(1));
+    assertEquals(new Vector2D(0, 1), points.get(2));
+    assertEquals(new Vector2D(0, 1), points.get(3));
+
+    points = spacialHashMap.getBucketIdsForEntity(entities.get(4));
+    assertEquals(new Vector2D(1, 0), points.get(0));
+    assertEquals(new Vector2D(1, 0), points.get(1));
+    assertEquals(new Vector2D(1, 1), points.get(2));
+    assertEquals(new Vector2D(1, 1), points.get(3));
+  }
+
+  @Test
+  void whenEntitiesRegistered_shouldHaveProperNearbyEntities() {
+    World world = createTestWorld();
+    List<Entity> entities = world.getEntitiesWithComponents();
+    SpacialHashMap spacialHashMap = new SpacialHashMap(entities);
+
+    List<Entity> nearbyEntities = spacialHashMap.getNearbyEntities(entities.get(0));
+    assertTrue(nearbyEntities.contains(entities.get(1)));
+    assertTrue(nearbyEntities.contains(entities.get(3)));
+
+    nearbyEntities = spacialHashMap.getNearbyEntities(entities.get(1));
+    assertTrue(nearbyEntities.contains(entities.get(0)));
+    assertTrue(nearbyEntities.contains(entities.get(3)));
+    assertTrue(nearbyEntities.contains(entities.get(4)));
+
+    nearbyEntities = spacialHashMap.getNearbyEntities(entities.get(2));
+    assertTrue(nearbyEntities.contains(entities.get(1)));
+    assertTrue(nearbyEntities.contains(entities.get(4)));
+
+
+    nearbyEntities = spacialHashMap.getNearbyEntities(entities.get(3));
+    assertTrue(nearbyEntities.contains(entities.get(0)));
+    assertTrue(nearbyEntities.contains(entities.get(1)));
+
+    nearbyEntities = spacialHashMap.getNearbyEntities(entities.get(4));
+    assertTrue(nearbyEntities.contains(entities.get(1)));
+    assertTrue(nearbyEntities.contains(entities.get(2)));
+  }
+
+  private World createTestWorld() {
+    World world = new World(5);
+
+    world.createEntity()
+      .addComponent(new PositionComponent(0, 0))
+      .addComponent(ColliderComponent.rectangle(10, 10));
+
+    world.createEntity()
+      .addComponent(new PositionComponent(25, 25))
+      .addComponent(ColliderComponent.rectangle(20, 20));
+
+    world.createEntity()
+      .addComponent(new PositionComponent(55, 55))
+      .addComponent(ColliderComponent.rectangle(15, 15));
+
+    world.createEntity()
+      .addComponent(new PositionComponent(25, 35))
+      .addComponent(ColliderComponent.rectangle(10, 10));
+
+    world.createEntity()
+      .addComponent(new PositionComponent(45, 35))
+      .addComponent(ColliderComponent.rectangle(10, 10));
+
+    return world;
+  }
+}
+
