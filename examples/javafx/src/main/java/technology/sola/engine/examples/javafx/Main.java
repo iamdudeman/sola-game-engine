6d6bb5bf9c2d3d3c3ef25b261bd2fb931da0f939
Tim Solum <timothy.solum@gmail.com>
2021-05-17 11:36:53 -0500

    cleanup

--- examples/javafx/src/main/java/technology/sola/engine/examples/javafx/Main.java
+++ examples/javafx/src/main/java/technology/sola/engine/examples/javafx/Main.java
@@ -1,13 +1,12 @@
 package technology.sola.engine.examples.javafx;
 
-import technology.sola.engine.core.AbstractSolaPlatform;
 import technology.sola.engine.examples.common.game.TestGame;
 import technology.sola.engine.platform.javafx.SolaJavaFxPlatform;
 
 public class Main {
   public static void main(String[] args) {
-    TestGame testGame = new TestGame();
-    AbstractSolaPlatform solaPlatform = new SolaJavaFxPlatform("JavaFX Test", 800, 600);
+    var testGame = new TestGame();
+    var solaPlatform = new SolaJavaFxPlatform("JavaFX Test", 800, 600);
 
     solaPlatform.launch(testGame);
   }
