489e3ff6f48c525fbf693c70841cdadd938b7a7f
iamdudeman <timothy.solum@gmail.com>
2021-05-28 17:23:08 -0500

    Cleaned up examples:browser gradle file

--- examples/browser/build.gradle.kts
+++ examples/browser/build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
   }
 
   dependencies {
-    classpath("io.github.zebalu:teavm-gradle-plugin:+")
+    classpath("io.github.zebalu:teavm-gradle-plugin:1.0.0")
   }
 }
 
@@ -30,19 +30,6 @@ teavm {
 
   isMinifying = false
   targetType = org.teavm.tooling.TeaVMTargetType.JAVASCRIPT
-}
-
-
-tasks.withType<Jar>() {
-  manifest {
-    attributes["Main-Class"] = "technology.sola.engine.examples.Main"
-  }
-
-  dependsOn(configurations.runtimeClasspath)
-
-  from({
-    configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-  })
 }
 
 tasks.withType<io.github.zebalu.gradle.teavm.TeavmCompileTask> {
