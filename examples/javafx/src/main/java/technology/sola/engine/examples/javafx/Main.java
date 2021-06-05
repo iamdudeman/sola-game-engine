e3edabbed3e5afe4152692cd80b29d303dbc9e22
iamdudeman <timothy.solum@gmail.com>
2021-06-04 19:08:41 -0500

    More cleanup

--- examples/javafx/src/main/java/technology/sola/engine/examples/javafx/Main.java
+++ examples/javafx/src/main/java/technology/sola/engine/examples/javafx/Main.java
@@ -1,13 +1,11 @@
 package technology.sola.engine.examples.javafx;
 
-import technology.sola.engine.examples.common.singlefile.RenderingExample;
 import technology.sola.engine.examples.common.singlefile.StressTestExample;
 import technology.sola.engine.platform.javafx.JavaFxSolaPlatform;
 
 public class Main {
   public static void main(String[] args) {
-//    var sola = new StressTestExample(1600);
-    var sola = new RenderingExample();
+    var sola = new StressTestExample(1600);
     var solaPlatform = new JavaFxSolaPlatform("JavaFX Test");
 
     solaPlatform.launch(sola);
--- platform/swing/src/main/java/technology/sola/engine/platform/swing/SolaImageAssetPool.java
+++ platform/swing/src/main/java/technology/sola/engine/platform/swing/SolaImageAssetPool.java
@@ -2,6 +2,7 @@ package technology.sola.engine.platform.swing;
 
 import technology.sola.engine.assets.AssetPool;
 import technology.sola.engine.graphics.SolaImage;
+import technology.sola.engine.platform.swing.exception.FailedSolaImageLoadException;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -19,16 +20,13 @@ public class SolaImageAssetPool extends AssetPool<SolaImage> {
     try {
       File file = new File(path);
       BufferedImage bufferedImage = ImageIO.read(file);
-
       SolaImage solaImage = new SolaImage(bufferedImage.getWidth(), bufferedImage.getHeight());
+
       bufferedImage.getRGB(0, 0, solaImage.getWidth(), solaImage.getHeight(), solaImage.getPixels(), 0, solaImage.getWidth());
 
       return solaImage;
     } catch (IOException ex) {
-      // todo
-      ex.printStackTrace();
+      throw new FailedSolaImageLoadException(path);
     }
-
-    return null;
   }
 }
