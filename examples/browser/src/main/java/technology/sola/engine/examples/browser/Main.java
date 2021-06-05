6f6ade8d75794aade4df3417141013fbe9de1605
iamdudeman <timothy.solum@gmail.com>
2021-06-04 19:24:00 -0500

    More cleanup

--- examples/browser/src/main/java/technology/sola/engine/examples/browser/Main.java
+++ examples/browser/src/main/java/technology/sola/engine/examples/browser/Main.java
@@ -1,12 +1,11 @@
 package technology.sola.engine.examples.browser;
 
-import technology.sola.engine.examples.common.singlefile.RenderingExample;
 import technology.sola.engine.examples.common.singlefile.SimplePlatformerExample;
 import technology.sola.engine.platform.browser.BrowserSolaPlatform;
 
 public class Main {
   public static void main(String[] args) {
-    var sola = new RenderingExample();
+    var sola = new SimplePlatformerExample();
     var solaPlatform = new BrowserSolaPlatform("Sola Browser Test");
 
     solaPlatform.launch(sola);
