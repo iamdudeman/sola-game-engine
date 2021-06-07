5a6345d5d0f85317c0577c4361c39cefe9a2960a
iamdudeman <timothy.solum@gmail.com>
2021-06-06 20:10:54 -0500

    Can now rasterize ASCII characters in desired font, font style and font size

--- tools/src/main/java/technology/sola/engine/tools/executable/FontRasterizerExecutable.java
+++ tools/src/main/java/technology/sola/engine/tools/executable/FontRasterizerExecutable.java
@@ -8,48 +8,79 @@ import java.io.File;
 import java.io.IOException;
 
 public class FontRasterizerExecutable implements ToolExecutable {
-  @Override
-  public void execute(String[] toolArgs) {
-    Font font = new Font("monospaced", Font.PLAIN, 12);
+  private enum FontStyle {
+    NORMAL(0),
+    BOLD(1),
+    ITALIC(2),
+    ;
 
-    BufferedImage bufferedImage = new BufferedImage(100, 80, BufferedImage.TYPE_INT_ARGB);
+    public static FontStyle valueOf(int code) {
+      for (FontStyle fontStyle : values()) {
+        if (fontStyle.code == code) {
+          return fontStyle;
+        }
+      }
 
-    Graphics2D graphics = (Graphics2D) bufferedImage.getGraphics();
-
-
-    graphics.setColor(Color.WHITE);
-    graphics.fillRect(0, 0, 100, 80);
-
-
-    graphics.setColor(Color.BLACK);
-    graphics.setFont(font);
-//    graphics.drawString("ABCDEFG", 0, 0);
-
-    FontMetrics fontMetrics = graphics.getFontMetrics();
-
-    String[] characters = new String[] {
-      "a", "b", "c", "d"
-    };
-    int widthCounter = 0;
-    int heightCounter = 0;
-    final int margin = 1;
-
-    for (String character : characters) {
-      Rectangle2D rectangle = fontMetrics.getStringBounds(character, graphics);
-
-      heightCounter += rectangle.getHeight() + margin;
-
-      graphics.drawString(character, widthCounter, heightCounter);
-
-      widthCounter += rectangle.getWidth() + margin;
+      throw new IllegalArgumentException("FontStyle code [" + code + "] is invalid");
     }
 
-//    graphics.drawString("ABCDEFG", 0, fontMetrics.getHeight());
-//    graphics.drawString("abcdefg", 0, fontMetrics.getHeight() * 2);
+    private final int code;
+
+    FontStyle(int code) {
+      this.code = code;
+    }
+
+    public int getCode() {
+      return code;
+    }
+  }
+
+  @Override
+  public void execute(String[] toolArgs) {
+    String fontName = "monospaced";
+//    String fontName = "times";
+    FontStyle fontStyle = FontStyle.NORMAL;
+    int fontSize = 16;
+
+    String characters = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+
+    // logic below here
+
+    int imageWidth = fontSize * 10;
+    int imageHeight = fontSize * characters.length() / 10;
+    final int margin = 1;
+
+    BufferedImage bufferedImage = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D graphics = (Graphics2D) bufferedImage.getGraphics();
+    Font font = new Font(fontName, fontStyle.getCode(), fontSize);
+
+    FontMetrics fontMetrics = graphics.getFontMetrics(font);
+    graphics.setColor(Color.BLACK);
+    graphics.setFont(font);
+
+    int x = 0;
+    int y = fontMetrics.getMaxAscent();
+
+    for (String character : characters.split("")) {
+      Rectangle2D rectangle = fontMetrics.getStringBounds(character, graphics);
+      int characterWidth = (int) rectangle.getWidth();
+      int characterHeight = fontMetrics.getAscent();
+
+      if (x + characterWidth >= imageWidth) {
+        x = 0;
+        y += fontSize + margin;
+      }
+
+      System.out.println(character + " at " + x + " " + y + " width " + characterWidth + " height " + characterHeight);
+      graphics.drawString(character, x, y);
+
+      x += characterWidth + margin;
+    }
 
     graphics.dispose();
 
-    File file = new File("font_test.png");
+    File file = new File(fontName + "_" + fontStyle.name() + "_" + fontSize + ".png");
 
     try {
       ImageIO.write(bufferedImage, "png", file);
