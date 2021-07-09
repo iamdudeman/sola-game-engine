3413f5e3e88cba939a42ac6ac3294f82163d8de3
iamdudeman <timothy.solum@gmail.com>
2021-07-08 19:37:36 -0500

    Added flag to prevent unneeded recalculation of inverse of matrix

--- engine/src/main/java/technology/sola/engine/graphics/AffineTransform.java
+++ engine/src/main/java/technology/sola/engine/graphics/AffineTransform.java
@@ -7,29 +7,39 @@ import technology.sola.math.linear.Vector2D;
 public class AffineTransform {
   private Matrix3D matrix3D = Matrix3D.identity();
   private Matrix3D invertedMatrix3D = Matrix3D.identity();
+  private boolean isDirty = false;
 
   public void reset() {
     matrix3D = Matrix3D.identity();
+    invertedMatrix3D = matrix3D;
+    isDirty = false;
   }
 
   public void translate(float tx, float ty) {
     matrix3D = matrix3D.multiply(Matrix3D.translate(tx, ty));
+    isDirty = true;
   }
 
   public void scale(float sx, float sy) {
     matrix3D = matrix3D.multiply(Matrix3D.scale(sx, sy));
+    isDirty = true;
   }
 
   public void rotate(float radians) {
     matrix3D = matrix3D.multiply(Matrix3D.rotate(radians));
+    isDirty = true;
   }
 
   public void sheer(float sx, float sy) {
     matrix3D = matrix3D.multiply(Matrix3D.sheer(sx, sy));
+    isDirty = true;
   }
 
   public void invert() {
+    if (!isDirty) return;
+
     invertedMatrix3D = matrix3D.invert();
+    isDirty = false;
   }
 
   public Vector2D forward(float x, float y) {
