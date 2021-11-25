8b2271d755e3b2d708b70024477a3344464b8e4f
iamdudeman <timothy.solum@gmail.com>
2021-11-25 10:59:00 -0600

    Added basic FileTreeItem component for AssetsScreen

--- editor/src/main/java/technology/sola/engine/editor/controllers/AssetsScreenController.java
+++ editor/src/main/java/technology/sola/engine/editor/controllers/AssetsScreenController.java
@@ -2,14 +2,22 @@ package technology.sola.engine.editor.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import technology.sola.engine.editor.JavaFxController;
 import technology.sola.engine.editor.SolaEditorContext;
+import technology.sola.engine.editor.components.FileTreeItem;
 import technology.sola.engine.editor.dialog.NewFontDialog;
+
+import java.io.File;
 
 public class AssetsScreenController implements JavaFxController {
   @FXML
   private MenuItem menuItemCreateFont;
+
+  @FXML
+  private VBox container;
 
   private final Stage owner;
   private final SolaEditorContext solaEditorContext;
@@ -26,6 +34,12 @@ public class AssetsScreenController implements JavaFxController {
 
   @Override
   public void initialize() {
+    TreeView<File> fileView = new TreeView<>(
+      new FileTreeItem(new File(solaEditorContext.projectFilePropertyProperty().getValue().getParentFile(), "assets"))
+    );
+
+    container.getChildren().add(fileView);
+
     menuItemCreateFont.setOnAction(event -> {
       new NewFontDialog(owner, solaEditorContext).showAndWait();
     });
