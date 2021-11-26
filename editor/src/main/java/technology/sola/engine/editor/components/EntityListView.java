322b4ac18a0040e17284fd2c3f513a90aa571820
iamdudeman <timothy.solum@gmail.com>
2021-11-26 17:23:04 -0600

    Basic world save / load functioning

---
+++ editor/src/main/java/technology/sola/engine/editor/components/EntityListView.java
@@ -0,0 +1,8 @@
+package technology.sola.engine.editor.components;
+
+import javafx.scene.control.ListView;
+import technology.sola.engine.ecs.Entity;
+
+public class EntityListView extends ListView<Entity> {
+
+}
--- editor/src/main/java/technology/sola/engine/editor/screens/world/WorldScreenController.java
+++ editor/src/main/java/technology/sola/engine/editor/screens/world/WorldScreenController.java
@@ -1,11 +1,19 @@
 package technology.sola.engine.editor.screens.world;
 
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import technology.sola.engine.ecs.Entity;
 import technology.sola.engine.ecs.World;
 import technology.sola.engine.ecs.io.Base64WorldSerializer;
+import technology.sola.engine.editor.components.EntityListView;
 import technology.sola.engine.editor.core.FolderUtils;
 import technology.sola.engine.editor.core.SolaEditorContext;
 import technology.sola.engine.editor.screens.SolaEditorScreen;
@@ -14,13 +22,28 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorldScreenController implements SolaEditorScreen {
   private final Stage owner;
   private final SolaEditorContext solaEditorContext;
+  private final Property<World> worldProperty = new SimpleObjectProperty<>(null);
+  private final Property<File> worldFileProperty = new SimpleObjectProperty<>(null);
+  private ObservableList<Entity> entityList = FXCollections.observableList(new ArrayList<>());
 
   @FXML
+  private VBox container;
+  @FXML
   private MenuItem menuItemNew;
+  @FXML
+  private MenuItem menuItemOpen;
+  @FXML
+  private MenuItem menuItemSave;
+  @FXML
+  private Menu menuWorld;
+  @FXML
+  private MenuItem menuItemNewEntity;
 
   public WorldScreenController(Stage owner, SolaEditorContext solaEditorContext) {
     this.owner = owner;
@@ -42,24 +65,79 @@ public class WorldScreenController implements SolaEditorScreen {
     fileChooser.getExtensionFilters().add(solaExtensionFilter);
     fileChooser.setSelectedExtensionFilter(solaExtensionFilter);
 
+    EntityListView entityListView = new EntityListView();
+    container.getChildren().add(entityListView);
+
+    entityListView.setItems(entityList);
+
+    worldProperty.addListener(((observable, oldValue, newValue) -> {
+      menuWorld.setDisable(newValue == null);
+      menuItemSave.setDisable(newValue == null);
+    }));
 
     menuItemNew.setOnAction(event -> {
       fileChooser.setInitialFileName("new_world.world");
 
       File worldFile = fileChooser.showSaveDialog(owner);
 
+      worldFileProperty.setValue(worldFile);
+
       try {
         if (worldFile != null) {
           worldFile.createNewFile();
 
-          String stuff = new Base64WorldSerializer().stringify(new World(10));
+          worldProperty.setValue(new World(100));
 
-          Files.write(worldFile.toPath(), stuff.getBytes(StandardCharsets.UTF_8));
+          saveWorld();
         }
       } catch (IOException e) {
         e.printStackTrace();
       }
     });
 
+    menuItemOpen.setOnAction(event -> {
+      fileChooser.setInitialFileName(null);
+
+      File worldFile = fileChooser.showOpenDialog(owner);
+
+      if (worldFile != null) {
+        worldFileProperty.setValue(worldFile);
+        try {
+          loadWorld();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+
+    menuItemSave.setOnAction(event -> {
+      try {
+        saveWorld();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    });
+
+    menuItemNewEntity.setOnAction(event -> {
+      Entity entity = worldProperty.getValue()
+        .createEntity()
+        .setName("New");
+
+      entityList.add(entity);
+    });
+  }
+
+  private void saveWorld() throws IOException {
+    String serializedWorld = new Base64WorldSerializer().stringify(worldProperty.getValue());
+
+    Files.write(worldFileProperty.getValue().toPath(), serializedWorld.getBytes(StandardCharsets.UTF_8));
+  }
+
+  private void loadWorld() throws IOException {
+    String serializedWorld = Files.readString(worldFileProperty.getValue().toPath());
+
+    worldProperty.setValue(new Base64WorldSerializer().parse(serializedWorld));
+    entityList.clear();
+    entityList.addAll(worldProperty.getValue().getEntitiesWithComponents());
   }
 }
