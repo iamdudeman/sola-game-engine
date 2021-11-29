81bb8e0b225625539256164b00b278c12e37bf75
iamdudeman <timothy.solum@gmail.com>
2021-11-29 15:36:46 -0600

    added some todos

--- editor/src/main/java/technology/sola/engine/editor/components/ecs/ComponentController.java
+++ editor/src/main/java/technology/sola/engine/editor/components/ecs/ComponentController.java
@@ -2,6 +2,9 @@ package technology.sola.engine.editor.components.ecs;
 
 import technology.sola.engine.ecs.Entity;
 
+// TODO find a better shared abstraction for component controllers
+// TODO figure out how to have the UI dynamically create menus for available component types
+
 public interface ComponentController<T> {
   String getFxmlResource();
 
