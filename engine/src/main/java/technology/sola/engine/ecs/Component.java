363f23db14bb903909986ef894a2d99dde4383e1
iamdudeman <timothy.solum@gmail.com>
2021-12-30 12:56:56 -0600

    Live preview now uses a copy world instead of saving + reloading

--- engine/src/main/java/technology/sola/engine/ecs/Component.java
+++ engine/src/main/java/technology/sola/engine/ecs/Component.java
@@ -2,6 +2,6 @@ package technology.sola.engine.ecs;
 
 import java.io.Serializable;
 
-public interface Component<T> extends Serializable {
+public interface Component<T extends Component> extends Serializable {
   T copy();
 }
