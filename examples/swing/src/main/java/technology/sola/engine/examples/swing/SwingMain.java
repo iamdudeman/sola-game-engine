6848c53a62353b6aec3868d2821057e84cfe25b9
iamdudeman <timothy.solum@gmail.com>
2022-01-15 22:55:15 -0600

    SpriteSheetAssetPool implementations now use SolaJson

--- examples/swing/src/main/java/technology/sola/engine/examples/swing/SwingMain.java
+++ examples/swing/src/main/java/technology/sola/engine/examples/swing/SwingMain.java
@@ -3,7 +3,6 @@ package technology.sola.engine.examples.swing;
 import technology.sola.engine.core.SolaPlatform;
 import technology.sola.engine.core.Sola;
 import technology.sola.engine.examples.common.singlefile.RenderingExample;
-import technology.sola.engine.examples.common.singlefile.SimplePlatformerExample;
 import technology.sola.engine.platform.swing.SwingSolaPlatform;
 
 public class SwingMain {
