36e5e35dac01654a92c76b001eac545e40ac353d
iamdudeman <timothy.solum@gmail.com>
2022-01-16 22:17:55 -0600

    FontInfo and FontGlyph are now records. FontAssetPool for JavaFx and Swing now use SolaJson.

--- engine/src/main/java/technology/sola/engine/graphics/font/FontGlyph.java
+++ engine/src/main/java/technology/sola/engine/graphics/font/FontGlyph.java
@@ -1,38 +1,24 @@
 package technology.sola.engine.graphics.font;
 
-// TODO convert to record if we don't use GSON for deserializing
-public class FontGlyph {
-  private final char glyph;
-  private final int x;
-  private final int y;
-  private final int width;
-  private final int height;
+import technology.sola.json.JsonMapper;
+import technology.sola.json.JsonObject;
 
-  public FontGlyph(char glyph, int x, int y, int width, int height) {
-    this.glyph = glyph;
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
+public record FontGlyph(char glyph, int x, int y, int width, int height) {
+  public static final JsonMapper<FontGlyph> JSON_MAPPER = new JsonMapper<>() {
+    @Override
+    public JsonObject toJson(FontGlyph fontGlyph) {
+      throw new UnsupportedOperationException();
+    }
 
-  public char getGlyph() {
-    return glyph;
-  }
-
-  public int getX() {
-    return x;
-  }
-
-  public int getY() {
-    return y;
-  }
-
-  public int getWidth() {
-    return width;
-  }
-
-  public int getHeight() {
-    return height;
-  }
+    @Override
+    public FontGlyph toObject(JsonObject jsonObject) {
+      return new FontGlyph(
+        jsonObject.getString("glyph").charAt(0),
+        jsonObject.getInt("x"),
+        jsonObject.getInt("y"),
+        jsonObject.getInt("width"),
+        jsonObject.getInt("height")
+      );
+    }
+  };
 }
--- engine/src/main/java/technology/sola/engine/graphics/font/FontInfo.java
+++ engine/src/main/java/technology/sola/engine/graphics/font/FontInfo.java
@@ -1,40 +1,35 @@
 package technology.sola.engine.graphics.font;
 
+import technology.sola.json.JsonMapper;
+import technology.sola.json.JsonObject;
+
 import java.util.List;
 
-public class FontInfo {
-  private String fontGlyphFile;
-  private String fontName;
-  private FontStyle fontStyle;
-  private int fontSize;
-  private int leading;
-  private List<FontGlyph> glyphs;
+public record FontInfo(
+  String fontGlyphFile, String fontName,
+  FontStyle fontStyle, int fontSize, int leading,
+  List<FontGlyph> glyphs) {
 
-  public FontGlyph getFontGlyph(char character) {
-    return glyphs.stream().filter(fontGlyph -> fontGlyph.getGlyph() == character).findFirst().orElse(null);
-  }
+  public static final JsonMapper<FontInfo> JSON_MAPPER = new JsonMapper<>() {
+    @Override
+    public JsonObject toJson(FontInfo fontInfo) {
+      throw new UnsupportedOperationException();
+    }
 
-  public String getFontGlyphFile() {
-    return fontGlyphFile;
-  }
+    @Override
+    public FontInfo toObject(JsonObject jsonObject) {
+      List<FontGlyph> glyphs = jsonObject.getArray("glyphs")
+        .stream().map(jsonGlyph -> FontGlyph.JSON_MAPPER.toObject(jsonGlyph.asObject()))
+        .toList();
 
-  public String getFontName() {
-    return fontName;
-  }
-
-  public FontStyle getFontStyle() {
-    return fontStyle;
-  }
-
-  public int getFontSize() {
-    return fontSize;
-  }
-
-  public int getLeading() {
-    return leading;
-  }
-
-  public List<FontGlyph> getGlyphs() {
-    return glyphs;
-  }
+      return new FontInfo(
+        jsonObject.getString("fontGlyphFile"),
+        jsonObject.getString("fontName"),
+        FontStyle.valueOf(jsonObject.getString("fontStyle")),
+        jsonObject.getInt("fontSize"),
+        jsonObject.getInt("leading"),
+        glyphs
+      );
+    }
+  };
 }
