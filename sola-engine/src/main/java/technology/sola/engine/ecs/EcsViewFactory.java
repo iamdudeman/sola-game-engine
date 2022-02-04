d352a7ff4955b91deb5d953e28e49bbd6b6e08cc
iamdudeman <timothy.solum@gmail.com>
2022-02-04 08:33:55 -0600

    Added EcsView5

--- sola-engine/src/main/java/technology/sola/engine/ecs/EcsViewFactory.java
+++ sola-engine/src/main/java/technology/sola/engine/ecs/EcsViewFactory.java
@@ -4,6 +4,7 @@ import technology.sola.engine.ecs.view.EcsView1;
 import technology.sola.engine.ecs.view.EcsView2;
 import technology.sola.engine.ecs.view.EcsView3;
 import technology.sola.engine.ecs.view.EcsView4;
+import technology.sola.engine.ecs.view.EcsView5;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -63,7 +64,10 @@ public class EcsViewFactory {
     return view;
   }
 
-  public final <C1 extends Component<?>, C2 extends Component<?>, C3 extends Component<?>> List<EcsView3<C1, C2, C3>> of(Class<C1> c1Class, Class<C2> c2Class, Class<C3> c3Class) {
+  public final <C1 extends Component<?>, C2 extends Component<?>, C3 extends Component<?>>
+  List<EcsView3<C1, C2, C3>> of(
+    Class<C1> c1Class, Class<C2> c2Class, Class<C3> c3Class
+  ) {
     List<EcsView3<C1, C2, C3>> view = new ArrayList<>(entities.length);
 
     for (Entity entity : entities) {
@@ -82,7 +86,10 @@ public class EcsViewFactory {
     return view;
   }
 
-  public final <C1 extends Component<?>, C2 extends Component<?>, C3 extends Component<?>, C4 extends Component<?>> List<EcsView4<C1, C2, C3, C4>> of(Class<C1> c1Class, Class<C2> c2Class, Class<C3> c3Class, Class<C4> c4Class) {
+  public final <C1 extends Component<?>, C2 extends Component<?>, C3 extends Component<?>, C4 extends Component<?>>
+  List<EcsView4<C1, C2, C3, C4>> of(
+    Class<C1> c1Class, Class<C2> c2Class, Class<C3> c3Class, Class<C4> c4Class
+  ) {
     List<EcsView4<C1, C2, C3, C4>> view = new ArrayList<>(entities.length);
 
     for (Entity entity : entities) {
@@ -96,6 +103,30 @@ public class EcsViewFactory {
 
       if (c1 != null && c2 != null && c3 != null && c4 != null) {
         view.add(new EcsView4<>(entity, c1, c2, c3, c4));
+      }
+    }
+
+    return view;
+  }
+
+  public final <C1 extends Component<?>, C2 extends Component<?>, C3 extends Component<?>, C4 extends Component<?>, C5 extends Component<?>>
+  List<EcsView5<C1, C2, C3, C4, C5>> of(
+    Class<C1> c1Class, Class<C2> c2Class, Class<C3> c3Class, Class<C4> c4Class, Class<C5> c5class
+  ) {
+    List<EcsView5<C1, C2, C3, C4, C5>> view = new ArrayList<>(entities.length);
+
+    for (Entity entity : entities) {
+      if (entity == null || entity.isDisabled()) continue;
+
+      var entityIndex = entity.getIndexInWorld();
+      var c1 = world.getComponentForEntity(entityIndex, c1Class);
+      var c2 = world.getComponentForEntity(entityIndex, c2Class);
+      var c3 = world.getComponentForEntity(entityIndex, c3Class);
+      var c4 = world.getComponentForEntity(entityIndex, c4Class);
+      var c5 = world.getComponentForEntity(entityIndex, c5class);
+
+      if (c1 != null && c2 != null && c3 != null && c4 != null && c5 != null) {
+        view.add(new EcsView5<>(entity, c1, c2, c3, c4, c5));
       }
     }
 
