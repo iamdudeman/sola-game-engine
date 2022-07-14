bae40470c0bcd725aa41594601715089c0df25a3
iamdudeman <timothy.solum@gmail.com>
2022-07-14 10:12:03 -0500

    Removed OldSolaGui approach

--- sola-engine/src/main/java/technology/sola/engine/graphics/gui/oldway/GuiElementMouseEventCallback.java
+++
@@ -1,8 +0,0 @@
-package technology.sola.engine.graphics.gui.oldway;
-
-import technology.sola.engine.input.MouseEvent;
-
-@Deprecated
-public interface GuiElementMouseEventCallback {
-  void call(MouseEvent mouseEvent);
-}
