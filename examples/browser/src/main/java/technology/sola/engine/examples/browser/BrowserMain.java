cb85cdd1b596c3982b6366bcbbeb21b7a4559fca
iamdudeman <timothy.solum@gmail.com>
2022-08-05 14:04:32 -0500

    Cleaned up JsKeyboardUtils scripts

--- examples/browser/src/main/java/technology/sola/engine/examples/browser/BrowserMain.java
+++ examples/browser/src/main/java/technology/sola/engine/examples/browser/BrowserMain.java
@@ -2,12 +2,13 @@ package technology.sola.engine.examples.browser;
 
 import technology.sola.engine.core.SolaPlatform;
 import technology.sola.engine.core.Sola;
+import technology.sola.engine.examples.common.singlefile.MouseAndCameraExample;
 import technology.sola.engine.examples.common.singlefile.SimplePlatformerExample;
 import technology.sola.engine.platform.browser.BrowserSolaPlatform;
 
 public class BrowserMain {
   public static void main(String[] args) {
-    Sola sola = new SimplePlatformerExample();
+    Sola sola = new MouseAndCameraExample();
     SolaPlatform solaPlatform = new BrowserSolaPlatform();
 
     solaPlatform.play(sola);
--- sola-engine/platform/browser/src/main/java/technology/sola/engine/platform/browser/javascript/JsKeyboardUtils.java
+++ sola-engine/platform/browser/src/main/java/technology/sola/engine/platform/browser/javascript/JsKeyboardUtils.java
@@ -5,8 +5,11 @@ import org.teavm.jso.JSFunctor;
 import org.teavm.jso.JSObject;
 
 public class JsKeyboardUtils {
-  private static final String KEY_EVENT_SCRIPT =
-    "window.addEventListener(eventName, function(event) { callback(event.keyCode); }, false);";
+  private static final String KEY_EVENT_SCRIPT = """
+    window.addEventListener(eventName, function(event) {
+      callback(event.keyCode);
+    }, false);
+    """;
 
   @JSBody(params = { "eventName", "callback" }, script = KEY_EVENT_SCRIPT)
   public static native void keyEventListener(String eventName, KeyEventCallback callback);
