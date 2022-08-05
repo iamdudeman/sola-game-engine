8640ebf38295df68fc3a5e78c549f3ed3d47a3c5
iamdudeman <timothy.solum@gmail.com>
2022-08-05 13:52:55 -0500

    Added local TeaVM dependencies that support required missing features

--- sola-engine/platform/browser/build.gradle.kts
+++ sola-engine/platform/browser/build.gradle.kts
@@ -3,6 +3,16 @@ plugins {
 }
 
 dependencies {
+  /*
+  TODO replace with future TeaVM version from maven that supports required features
+   * records,
+   * Random#nextInt(origin, bounds)
+   * Random#nextFloat(origin, bounds)
+   * Thread#setName
+   */
+  implementation(files("libs/teavm-classlib-0.7.0-SNAPSHOT.jar"))
+  implementation(files("libs/teavm-core-0.7.0-SNAPSHOT.jar"))
+
   implementation("org.teavm:teavm-jso-apis:0.7.0")
 
   // Build script
