d2893997b29fd8f2374dc9d6096a22da1f8ce539
iamdudeman <timothy.solum@gmail.com>
2022-08-20 23:15:24 -0500

    Added description for bug that was discovered in platformer example

--- examples/javafx/src/main/java/technology/sola/engine/examples/javafx/JavaFxMain.java
+++ examples/javafx/src/main/java/technology/sola/engine/examples/javafx/JavaFxMain.java
@@ -3,13 +3,12 @@ package technology.sola.engine.examples.javafx;
 import technology.sola.engine.core.SolaPlatform;
 import technology.sola.engine.core.Sola;
 import technology.sola.engine.examples.common.ExampleLauncherSola;
-import technology.sola.engine.examples.common.singlefile.StressTestPhysicsExample;
 import technology.sola.engine.platform.javafx.JavaFxSolaPlatform;
 
 public class JavaFxMain {
   public static void main(String[] args) {
     SolaPlatform solaPlatform = new JavaFxSolaPlatform();
-    Sola sola = new StressTestPhysicsExample(2500);
+    Sola sola = new ExampleLauncherSola(solaPlatform);
 
     solaPlatform.play(sola);
   }
