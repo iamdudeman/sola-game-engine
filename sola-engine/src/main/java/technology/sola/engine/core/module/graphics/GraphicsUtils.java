9b9f0cfb405d4026b2a82c5fdd7049f48c5a0de2
iamdudeman <timothy.solum@gmail.com>
2022-09-15 14:27:03 -0500

    added entity culling for GeometryGraphics rendering

--- sola-engine/src/main/java/technology/sola/engine/core/module/graphics/GraphicsUtils.java
+++ sola-engine/src/main/java/technology/sola/engine/core/module/graphics/GraphicsUtils.java
@@ -1,6 +1,7 @@
 package technology.sola.engine.core.module.graphics;
 
 import technology.sola.engine.core.component.TransformComponent;
+import technology.sola.engine.graphics.renderer.Renderer;
 import technology.sola.math.linear.Matrix3D;
 import technology.sola.math.linear.Vector2D;
 
@@ -16,5 +17,25 @@ public class GraphicsUtils {
     return new TransformComponent(
       entityTranslation.x(), entityTranslation.y(), entityScale.x(), entityScale.y()
     );
+  }
+
+  public static boolean shouldCullEntity(Renderer renderer, float x, float y, float radius) {
+    return shouldCullEntity(renderer, x, y, radius, radius);
+  }
+
+  public static boolean shouldCullEntity(Renderer renderer, float x, float y, float width, float height) {
+    if (x - width > renderer.getWidth()) {
+      return true;
+    }
+
+    if (x + width < 0) {
+      return true;
+    }
+
+    if (y - height > renderer.getHeight()) {
+      return true;
+    }
+
+    return y + height < 0;
   }
 }
