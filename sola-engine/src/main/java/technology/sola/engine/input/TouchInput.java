a88a69e2c2ffd2de780de68a939d2b55aaa650eb
iamdudeman <timothy.solum@gmail.com>
2022-09-24 16:33:08 -0500

    Rough skeleton for TouchInput

---
+++ sola-engine/src/main/java/technology/sola/engine/input/TouchInput.java
@@ -0,0 +1,15 @@
+package technology.sola.engine.input;
+
+public class TouchInput {
+  public void onTouchMove(TouchEvent touchEvent) {
+    // todo
+  }
+
+  public void onTouchStart(TouchEvent touchEvent) {
+    // todo
+  }
+
+  public void onTouchEnd(TouchEvent touchEvent) {
+    // todo
+  }
+}
---
+++ sola-engine/src/main/java/technology/sola/engine/input/TouchPoint.java
@@ -0,0 +1,4 @@
+package technology.sola.engine.input;
+
+public record TouchPoint(long id, float x, float y) {
+}
