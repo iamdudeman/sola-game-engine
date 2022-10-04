9a93d0a0c8511caf4e96e1ac12e093149c0a0d4d
iamdudeman <timothy.solum@gmail.com>
2022-10-04 10:23:19 -0500

    Added a few JavaDocs for example files

--- examples/javafx/src/main/java/technology/sola/engine/examples/javafx/JavaFxMain.java
+++ examples/javafx/src/main/java/technology/sola/engine/examples/javafx/JavaFxMain.java
@@ -5,7 +5,15 @@ import technology.sola.engine.core.Sola;
 import technology.sola.engine.examples.common.ExampleLauncherSola;
 import technology.sola.engine.platform.javafx.JavaFxSolaPlatform;
 
+/**
+ * Runs the {@link ExampleLauncherSola} on the {@link JavaFxSolaPlatform}.
+ */
 public class JavaFxMain {
+  /**
+   * Entry point for JavaFX example.
+   *
+   * @param args command line args
+   */
   public static void main(String[] args) {
     SolaPlatform solaPlatform = new JavaFxSolaPlatform();
     Sola sola = new ExampleLauncherSola(solaPlatform);
