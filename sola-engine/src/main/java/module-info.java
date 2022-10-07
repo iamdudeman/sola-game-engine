8b21b8928f34e76eb5b55b7066cee0cf72ba1169
iamdudeman <timothy.solum@gmail.com>
2022-10-07 15:06:44 -0500

    Fixed checkstyle issue with modules. tooling project is now modularized.

--- sola-engine/src/main/java/module-info.java
+++ sola-engine/src/main/java/module-info.java
@@ -5,4 +5,9 @@ module technology.sola.engine {
   requires org.slf4j;
   requires transitive technology.sola.json;
   requires transitive technology.sola.ecs;
+
+  exports technology.sola.engine.assets;
+  exports technology.sola.engine.assets.graphics;
+  exports technology.sola.engine.assets.graphics.font;
+  exports technology.sola.engine.assets.graphics.font.mapper;
 }
