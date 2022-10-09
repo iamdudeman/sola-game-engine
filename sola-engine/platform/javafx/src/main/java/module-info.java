48a8c1a0a5c04d54f782a217f7789a1ae2c51f79
iamdudeman <timothy.solum@gmail.com>
2022-10-08 21:29:27 -0500

    Revert "Commenting out slf4j dependency for javafx platform"
    This reverts commit 66db8110edfe49ba684ee20dbed5e498c3aa4610.

--- sola-engine/platform/javafx/src/main/java/module-info.java
+++ sola-engine/platform/javafx/src/main/java/module-info.java
@@ -2,9 +2,9 @@
  * Defines the sola-game-engine Swing platform API.
  */
 module technology.sola.engine.platform.javafx {
-  // requires org.slf4j;
+  requires org.slf4j;
   requires java.desktop;
-  requires javafx.controls;
+  requires transitive javafx.controls;
   requires transitive technology.sola.engine;
 
   exports technology.sola.engine.platform.javafx;
