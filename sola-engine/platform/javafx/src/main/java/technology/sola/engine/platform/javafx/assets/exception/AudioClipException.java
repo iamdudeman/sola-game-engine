66db8110edfe49ba684ee20dbed5e498c3aa4610
iamdudeman <timothy.solum@gmail.com>
2022-10-08 20:32:02 -0500

    Commenting out slf4j dependency for javafx platform

--- sola-engine/platform/javafx/src/main/java/technology/sola/engine/platform/javafx/assets/exception/AudioClipException.java
+++ sola-engine/platform/javafx/src/main/java/technology/sola/engine/platform/javafx/assets/exception/AudioClipException.java
@@ -1,17 +1,17 @@
 package technology.sola.engine.platform.javafx.assets.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 import java.io.Serial;
 
 public class AudioClipException extends RuntimeException {
-  private static final Logger LOGGER = LoggerFactory.getLogger(AudioClipException.class);
+  // private static final Logger LOGGER = LoggerFactory.getLogger(AudioClipException.class);
   @Serial
   private static final long serialVersionUID = -3588454095968360166L;
 
   public AudioClipException(String message) {
     super(message);
-    LOGGER.error(message);
+    // LOGGER.error(message);
   }
 }
