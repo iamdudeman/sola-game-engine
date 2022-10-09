0196e2ae3aa70a73e66db5ad7cbd9817a0c6b2e3
iamdudeman <timothy.solum@gmail.com>
2022-10-08 20:19:32 -0500

    Added a missing javadoc

--- examples/javafx/src/main/java/module-info.java
+++ examples/javafx/src/main/java/module-info.java
@@ -1,3 +1,6 @@
+/**
+ * Defines the dependencies for the JavaFX example project.
+ */
 module technology.sola.engine.examples.javafx {
   requires technology.sola.engine.platform.javafx;
   requires technology.sola.engine.examples.common;
