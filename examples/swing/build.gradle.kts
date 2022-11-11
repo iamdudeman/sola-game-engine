71ad887f64419337a7ad41cf4e692254401afc9b
iamdudeman <timothy.solum@gmail.com>
2022-11-11 11:48:55 -0600

    Minor cleanup of swing distFatJar

--- examples/swing/build.gradle.kts
+++ examples/swing/build.gradle.kts
@@ -1,21 +1,19 @@
-plugins {
-  id("application")
-}
-
 dependencies {
   implementation(project(":sola-engine:platform:swing"))
   implementation(project(":examples:common"))
 }
 
-tasks.withType<Zip>() {
+tasks.withType<Zip> {
   duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
-tasks.withType<Tar>() {
+tasks.withType<Tar> {
   duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
-task("distFatJar", type = Jar::class) {
+// todo need to include assets
+// todo update code to check for assets internally first, then externally
+tasks.register("distFatJar", Jar::class) {
   group = "distribution"
   duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
@@ -28,6 +26,6 @@ task("distFatJar", type = Jar::class) {
   val dependencies = configurations.runtimeClasspath.get().map(::zipTree)
 
   from(dependencies)
-  dependsOn(configurations.runtimeClasspath)
   with(tasks.jar.get())
+  dependsOn(configurations.runtimeClasspath)
 }
