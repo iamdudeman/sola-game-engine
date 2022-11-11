deac33f692b9c4da4e38f2b0f51bcdfd966de463
iamdudeman <timothy.solum@gmail.com>
2022-11-11 10:33:24 -0600

    Added new distFatJar task for swing example

--- examples/swing/build.gradle.kts
+++ examples/swing/build.gradle.kts
@@ -2,14 +2,6 @@ plugins {
   id("application")
 }
 
-application {
-  mainClass.set("technology.sola.engine.examples.swing.SwingMain")
-}
-
-repositories {
-  mavenCentral()
-}
-
 dependencies {
   implementation(project(":sola-engine:platform:swing"))
   implementation(project(":examples:common"))
@@ -21,4 +13,21 @@ tasks.withType<Zip>() {
 
 tasks.withType<Tar>() {
   duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+task("distFatJar", type = Jar::class) {
+  group = "distribution"
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+  archiveBaseName.set("sola-engine-examples-${project.name}")
+
+  manifest {
+    attributes["Main-Class"] = "technology.sola.engine.examples.swing.SwingMain"
+  }
+
+  val dependencies = configurations.runtimeClasspath.get().map(::zipTree)
+
+  from(dependencies)
+  dependsOn(configurations.runtimeClasspath)
+  with(tasks.jar.get())
 }
