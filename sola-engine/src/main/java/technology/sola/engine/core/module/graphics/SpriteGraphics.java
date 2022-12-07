1d7f10e25c2838573476846d7650673f31b95a25
Timothy Solum <timothy.solum@gmail.com>
2022-12-06 20:04:53 -0600

    Feature/sprite graphics scale cache (#95)
    * added todo note for caching transformed sprites
    
    * Added basic caching mechanism to SolaImage for transforms

--- sola-engine/src/main/java/technology/sola/engine/core/module/graphics/SpriteGraphics.java
+++ sola-engine/src/main/java/technology/sola/engine/core/module/graphics/SpriteGraphics.java
@@ -3,9 +3,9 @@ package technology.sola.engine.core.module.graphics;
 import technology.sola.ecs.Entity;
 import technology.sola.ecs.SolaEcs;
 import technology.sola.engine.assets.AssetLoader;
+import technology.sola.engine.assets.graphics.SolaImage;
 import technology.sola.engine.assets.graphics.SpriteSheet;
 import technology.sola.engine.core.component.TransformComponent;
-import technology.sola.engine.graphics.AffineTransform;
 import technology.sola.engine.graphics.components.BlendModeComponent;
 import technology.sola.engine.graphics.components.LayerComponent;
 import technology.sola.engine.graphics.components.SpriteComponent;
@@ -36,20 +36,16 @@ class SpriteGraphics {
     spriteComponent.getSprite(spriteSheetAssetLoader).executeIfLoaded(sprite -> {
       BlendModeComponent blendModeComponent = entity.getComponent(BlendModeComponent.class);
       BlendMode previousBlendMode = renderer.getBlendMode();
+      SolaImage spriteImage = sprite;
       BlendMode blendMode = blendModeComponent == null ? previousBlendMode : blendModeComponent.getRenderMode();
 
       if (transformComponent.getScaleX() != 1 || transformComponent.getScaleY() != 1) {
-        AffineTransform affineTransform = new AffineTransform()
-          .translate(transformComponent.getX(), transformComponent.getY())
-          .scale(transformComponent.getScaleX(), transformComponent.getScaleY());
-
-        renderer.setBlendMode(BlendMode.MASK);
-        renderer.drawImage(sprite, affineTransform);
-      } else {
-        renderer.setBlendMode(blendMode);
-        renderer.drawImage(sprite, transformComponent.getX(), transformComponent.getY());
+        spriteImage = sprite.scale(transformComponent.getScaleX(), transformComponent.getScaleY());
+        blendMode = BlendMode.MASK;
       }
 
+      renderer.setBlendMode(blendMode);
+      renderer.drawImage(spriteImage, transformComponent.getX(), transformComponent.getY());
       renderer.setBlendMode(previousBlendMode);
     });
   }
