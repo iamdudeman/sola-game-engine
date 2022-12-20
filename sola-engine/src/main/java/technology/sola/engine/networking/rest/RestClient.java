918aa277b3be1b1e77c5967ff8cd871275b0ea67
iamdudeman <timothy.solum@gmail.com>
2022-12-19 20:57:35 -0600

    minor update

--- sola-engine/src/main/java/technology/sola/engine/networking/rest/RestClient.java
+++ sola-engine/src/main/java/technology/sola/engine/networking/rest/RestClient.java
@@ -1,5 +1,9 @@
 package technology.sola.engine.networking.rest;
 
+import java.util.function.Supplier;
+
 public interface RestClient {
-  HttpResponse send(HttpRequest httpRequest);
+  HttpResponse sendSync(HttpRequest httpRequest);
+
+  void send(HttpRequest httpRequest, Supplier<HttpResponse> httpResponseSupplier);
 }
