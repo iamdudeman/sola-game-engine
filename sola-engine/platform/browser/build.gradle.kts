e401daf4b9c61fb322af3c979d9089498e3572d3
Timothy Solum <timothy.solum@gmail.com>
2023-04-10 23:35:10 -0500

    Cleanup/update teavm and sola-ecs versions (#110)
    * updated teavm version to latest 0.8.0 release
    
    * updated teavm version to latest 0.8.1 release
    updated sola-ecs to latest 2.1.4 release

--- sola-engine/platform/browser/build.gradle.kts
+++ sola-engine/platform/browser/build.gradle.kts
@@ -2,13 +2,10 @@ plugins {
   id("sola.platform-conventions")
 }
 
-// Note: TeaVM:2b671b8088 contains needed features [records, Random#nextInt(origin, bounds), Random#nextFloat(origin, bounds), Thread#setName]
-val teavmVersion = "2b671b8088"
-
 dependencies {
-  implementation("com.github.konsoletyper.teavm:teavm-classlib:${teavmVersion}")
-  implementation("com.github.konsoletyper.teavm:teavm-tooling:${teavmVersion}")
-  implementation("com.github.konsoletyper.teavm:teavm-extras-slf4j:${teavmVersion}")
+  implementation("org.teavm:teavm-tooling:0.8.1")
+  implementation("org.teavm:teavm-classlib:0.8.1")
+  implementation("org.teavm:teavm-extras-slf4j:0.8.1")
 }
 
 publishing {
