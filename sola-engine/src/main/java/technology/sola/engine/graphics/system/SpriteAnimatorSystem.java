19ec0462494d8393754ef14108487aedcbcd72c0
iamdudeman <timothy.solum@gmail.com>
2023-07-08 15:12:03 -0500

    Added javadocs for graphics systems

--- sola-engine/src/main/java/technology/sola/engine/graphics/system/SpriteAnimatorSystem.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/system/SpriteAnimatorSystem.java
@@ -5,6 +5,10 @@ import technology.sola.ecs.World;
 import technology.sola.engine.graphics.components.animation.SpriteAnimatorComponent;
 import technology.sola.engine.graphics.components.SpriteComponent;
 
+/**
+ * SpriteAnimatorSystem handles updating the animation state of {@link technology.sola.ecs.Entity} that have a
+ * {@link SpriteComponent} and {@link SpriteAnimatorComponent}.
+ */
 public class SpriteAnimatorSystem extends EcsSystem {
   @Override
   public void update(World world, float deltaTime) {
