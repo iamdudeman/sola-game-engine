481c9ba6fd61a6e95d4df915454e621401fb49d3
iamdudeman <timothy.solum@gmail.com>
2023-07-16 18:49:53 -0500

    added more javadocs for some rendering related components

--- sola-engine/src/main/java/technology/sola/engine/graphics/components/CameraComponent.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/components/CameraComponent.java
@@ -2,13 +2,26 @@ package technology.sola.engine.graphics.components;
 
 import technology.sola.ecs.Component;
 
+/**
+ * CameraComponent is a {@link Component} that contains data for a 2d camera for an {@link technology.sola.ecs.Entity}.
+ */
 public class CameraComponent implements Component {
   private int priority = 0;
 
+  /**
+   * The camera with the lowest priority value will be used during rendering.
+   *
+   * @return the priority value
+   */
   public int getPriority() {
     return priority;
   }
 
+  /**
+   * Updates the priority for this camera. The camera with the lowest priority will be used during rendering.
+   *
+   * @param priority the new priority
+   */
   public void setPriority(int priority) {
     this.priority = priority;
   }
