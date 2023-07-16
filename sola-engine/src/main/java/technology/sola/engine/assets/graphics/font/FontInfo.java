e5d5f216e77725863a2da9ebdf8ba7d86451a5f6
iamdudeman <timothy.solum@gmail.com>
2023-07-16 15:17:06 -0500

    Fixed a small bug where FontInfo#leading was used for horizontal spacing instead of vertical spacing between lines

--- sola-engine/src/main/java/technology/sola/engine/assets/graphics/font/FontInfo.java
+++ sola-engine/src/main/java/technology/sola/engine/assets/graphics/font/FontInfo.java
@@ -2,6 +2,16 @@ package technology.sola.engine.assets.graphics.font;
 
 import java.util.List;
 
+/**
+ * FontInfo contains all the information needed to render strings for a {@link Font}.
+ *
+ * @param fontGlyphFile the file containing the rasterized glyphs
+ * @param fontFamily    the family of the font
+ * @param fontStyle     the style of the font (normal, bold, italic)
+ * @param fontSize      the size of the font
+ * @param leading       the logical amount of space to be reserved between the descent of one line of text and the ascent of the next line.
+ * @param glyphs        the {@link List} of {@link FontGlyph}s
+ */
 public record FontInfo(
   String fontGlyphFile, String fontFamily,
   FontStyle fontStyle, int fontSize, int leading,
