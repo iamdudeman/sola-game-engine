b886e0aa16e43855f3732d8c2e223120f18f5fb8
iamdudeman <timothy.solum@gmail.com>
2023-07-29 17:16:39 -0500

    Initial testing with BlendMode multiply making world darker

--- sola-engine/src/main/java/technology/sola/engine/defaults/graphics/modules/ScreenSpaceLightMapGraphicsModule.java
+++ sola-engine/src/main/java/technology/sola/engine/defaults/graphics/modules/ScreenSpaceLightMapGraphicsModule.java
@@ -2,12 +2,16 @@ package technology.sola.engine.defaults.graphics.modules;
 
 import technology.sola.ecs.World;
 import technology.sola.engine.core.component.TransformComponent;
+import technology.sola.engine.graphics.Color;
 import technology.sola.engine.graphics.components.LightComponent;
+import technology.sola.engine.graphics.renderer.BlendMode;
 import technology.sola.engine.graphics.renderer.Renderer;
 import technology.sola.math.linear.Matrix3D;
 
 public class ScreenSpaceLightMapGraphicsModule extends SolaGraphicsModule {
   public static final int ORDER = 99;
+
+  private Color baseDarkness = new Color(20, 20, 20);
 
   // todo need some sort of "base level darkness"
 
@@ -19,6 +23,11 @@ public class ScreenSpaceLightMapGraphicsModule extends SolaGraphicsModule {
     world.createView().of(TransformComponent.class, LightComponent.class);
 
     // todo render light map image with BlendMode to do color * light
+    BlendMode previousBlendMode = renderer.getBlendMode();
+
+    renderer.setBlendMode(BlendMode.MULTIPLY);
+    renderer.fillRect(0, 0, renderer.getWidth(), renderer.getHeight(), baseDarkness);
+    renderer.setBlendMode(previousBlendMode);
   }
 
   @Override
--- sola-engine/src/main/java/technology/sola/engine/graphics/components/LightComponent.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/components/LightComponent.java
@@ -1,6 +1,14 @@
 package technology.sola.engine.graphics.components;
 
 import technology.sola.ecs.Component;
+import technology.sola.engine.graphics.Color;
 
 public class LightComponent implements Component {
+  private Color lightColor;
+  private float radius;
+  private float intensity;
 }
+
+// todo light types
+//   Point (starts in a center and goes all around
+//   Spot angle + range
