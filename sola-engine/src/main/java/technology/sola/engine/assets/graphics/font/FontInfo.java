a04fa097b912a5f7adb30d204a0847082e685ce5
iamdudeman <timothy.solum@gmail.com>
2023-07-29 11:23:22 -0500

    Added more JavaDocs for font related code

--- sola-engine/src/main/java/technology/sola/engine/assets/graphics/font/FontInfo.java
+++ sola-engine/src/main/java/technology/sola/engine/assets/graphics/font/FontInfo.java
@@ -7,7 +7,7 @@ import java.util.List;
  *
  * @param fontGlyphFile the file containing the rasterized glyphs
  * @param fontFamily    the family of the font
- * @param fontStyle     the style of the font (normal, bold, italic)
+ * @param fontStyle     the {@link FontStyle} of the font
  * @param fontSize      the size of the font
  * @param leading       the logical amount of space to be reserved between the descent of one line of text and the ascent of the next line.
  * @param glyphs        the {@link List} of {@link FontGlyph}s
--- sola-engine/src/main/java/technology/sola/engine/assets/graphics/font/FontStyle.java
+++ sola-engine/src/main/java/technology/sola/engine/assets/graphics/font/FontStyle.java
@@ -1,19 +1,49 @@
 package technology.sola.engine.assets.graphics.font;
 
+/**
+ * FontStyle is an enum of possible {@link Font} style options. These are based on the
+ * <a href="https://docs.oracle.com/javase/8/docs/api/java/awt/Font.html">java.awt.Font</a> style options.
+ */
 public enum FontStyle {
-  NORMAL(0),
-  ITALIC(1),
-  BOLD(2),
-  BOLD_ITALIC(3),
+  /**
+   * Normal/plain styling.
+   */
+  NORMAL(0, "Normal"),
+  /**
+   * Italic styling.
+   */
+  ITALIC(1, "Italic"),
+  /**
+   * Bold styling.
+   */
+  BOLD(2, "Bold"),
+  /**
+   * Bold and italic styling.
+   */
+  BOLD_ITALIC(3, "Bold-italic"),
   ;
 
   private final int code;
+  private final String name;
 
-  FontStyle(int code) {
+  FontStyle(int code, String name) {
     this.code = code;
+    this.name = name;
   }
 
+  /**
+   * Returns the integer representation of the font style.
+   *
+   * @return the font style code
+   */
   public int getCode() {
     return code;
+  }
+
+  /**
+   * @return the name of the font style
+   */
+  public String getName() {
+    return name;
   }
 }
