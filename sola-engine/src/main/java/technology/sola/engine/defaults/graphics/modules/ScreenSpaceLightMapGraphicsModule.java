c5659765a6dcec51241b49a1c28635744d254d7e
iamdudeman <timothy.solum@gmail.com>
2023-07-29 21:41:32 -0500

    Hacked in a radial gradient for lights

--- sola-engine/src/main/java/technology/sola/engine/defaults/graphics/modules/ScreenSpaceLightMapGraphicsModule.java
+++ sola-engine/src/main/java/technology/sola/engine/defaults/graphics/modules/ScreenSpaceLightMapGraphicsModule.java
@@ -8,6 +8,7 @@ import technology.sola.engine.graphics.components.LightComponent;
 import technology.sola.engine.graphics.renderer.BlendMode;
 import technology.sola.engine.graphics.renderer.Renderer;
 import technology.sola.math.linear.Matrix3D;
+import technology.sola.math.linear.Vector2D;
 
 public class ScreenSpaceLightMapGraphicsModule extends SolaGraphicsModule {
   public static final int ORDER = 99;
@@ -35,7 +36,7 @@ public class ScreenSpaceLightMapGraphicsModule extends SolaGraphicsModule {
 
       // todo figure out gradient to black here
 
-      lightImageRenderer.fillCircle(
+      renderGradient(lightImageRenderer,
         transformComponent.getX() - radius + lightComponent.getOffsetX(),
         transformComponent.getY() - radius + lightComponent.getOffsetY(),
         radius,
@@ -61,5 +62,57 @@ public class ScreenSpaceLightMapGraphicsModule extends SolaGraphicsModule {
 
   public void setAmbientColor(Color ambientColor) {
     this.ambientColor = ambientColor;
+  }
+
+  // todo this is really hacky
+  private void renderGradient(Renderer renderer, float x, float y, float radius, Color color) {
+    if (shouldSkipDrawCall(renderer, x, y, radius)) {
+      return;
+    }
+
+    float centerX = x + radius;
+    float centerY = y + radius;
+
+    int xInt = (int) (x + radius + 0.5f);
+    int yInt = (int) (y + radius + 0.5f);
+    int radiusInt = (int) (radius + 0.5f);
+    int radiusSquaredInt = (int) (radius * radius + 0.5f);
+
+    for (int i = -radiusInt; i <= radius; i++) {
+      for (int j = -radiusInt; j <= radius; j++) {
+        if (j * j + i * i <= radiusSquaredInt) {
+          int px = xInt + j;
+          int py = yInt + i;
+          // todo hackkkyyyy
+          float doot = (radius - new Vector2D(px, py).distance(new Vector2D(centerX, centerY))) / radius;
+          int alpha = Math.round(doot * color.getAlpha());
+
+          Color newColor = new Color(alpha, color.getRed(), color.getGreen(), color.getBlue());
+
+          renderer.setPixel(px, py, newColor);
+        }
+      }
+    }
+  }
+
+  // todo these methods are copied from SoftwareRenderer
+  private boolean shouldSkipDrawCall(Renderer renderer, float x, float y, float radius) {
+    return shouldSkipDrawCall(renderer, x, y, radius, radius);
+  }
+
+  private boolean shouldSkipDrawCall(Renderer renderer, float x, float y, float width, float height) {
+    if (x - width > renderer.getWidth()) {
+      return true;
+    }
+
+    if (x + width < 0) {
+      return true;
+    }
+
+    if (y - height > renderer.getHeight()) {
+      return true;
+    }
+
+    return y + height < 0;
   }
 }
