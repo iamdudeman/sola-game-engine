fe1137b3ccd389a676e3a2eedb4bc1dac6d3c18f
iamdudeman <timothy.solum@gmail.com>
2023-08-02 21:00:56 -0500

    Added more javadocs

--- sola-engine/src/main/java/technology/sola/engine/graphics/gui/elements/input/ButtonGuiElement.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/gui/elements/input/ButtonGuiElement.java
@@ -7,18 +7,32 @@ import technology.sola.engine.input.MouseEvent;
 
 import java.util.function.Consumer;
 
+/**
+ * ButtonGuiElement is a {@link technology.sola.engine.graphics.gui.GuiElement} that provides a button input that can
+ * be clicked or interacted with via keyboard.
+ */
 public class ButtonGuiElement extends BaseInputGuiElement<ButtonGuiElement.Properties> {
-  private Runnable onActionConsumer = () -> {};
+  private Runnable onActionConsumer = () -> {
+  };
 
   private boolean wasMouseDownInside = false;
 
+  /**
+   * Creates a ButtonGuiElement instance.
+   *
+   * @param document the {@link SolaGuiDocument}
+   */
   public ButtonGuiElement(SolaGuiDocument document) {
     super(document, new Properties(document.propertyDefaults));
 
-    setOnMouseEnterCallback(event -> {});
-    setOnMouseExitCallback(event -> {});
-    setOnMouseDownCallback(event -> {});
-    setOnMouseUpCallback(event -> {});
+    setOnMouseEnterCallback(event -> {
+    });
+    setOnMouseExitCallback(event -> {
+    });
+    setOnMouseDownCallback(event -> {
+    });
+    setOnMouseUpCallback(event -> {
+    });
     setOnKeyPressCallback(keyEvent -> {
       int keyCode = keyEvent.getKeyCode();
 
@@ -57,6 +71,12 @@ public class ButtonGuiElement extends BaseInputGuiElement<ButtonGuiElement.Prope
     });
   }
 
+  /**
+   * Sets the action of this button.
+   *
+   * @param onActionConsumer the function ran when button is interacted with
+   * @return this
+   */
   public ButtonGuiElement setOnAction(Runnable onActionConsumer) {
     this.onActionConsumer = () -> {
       if (!properties().isDisabled()) {
@@ -67,7 +87,15 @@ public class ButtonGuiElement extends BaseInputGuiElement<ButtonGuiElement.Prope
     return this;
   }
 
+  /**
+   * The properties for a {@link ButtonGuiElement}.
+   */
   public static class Properties extends BaseInputGuiElement.Properties {
+    /**
+     * Creates the properties for a {@link ButtonGuiElement}.
+     *
+     * @param propertyDefaults the {@link GuiPropertyDefaults}
+     */
     public Properties(GuiPropertyDefaults propertyDefaults) {
       super(propertyDefaults);
     }
