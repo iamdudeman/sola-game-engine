215293f279fb2180e9f05615e9ee5ccb0b5840b9
iamdudeman <timothy.solum@gmail.com>
2023-09-04 14:38:48 -0500

    SoftwareRenderer now uses BlendFunction code

--- sola-engine/src/main/java/technology/sola/engine/graphics/renderer/BlendMode.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/renderer/BlendMode.java
@@ -1,44 +1,64 @@
 package technology.sola.engine.graphics.renderer;
 
+import technology.sola.engine.graphics.Color;
+import technology.sola.engine.graphics.renderer.blend.BlendFunction;
+import technology.sola.engine.graphics.renderer.blend.DissolveBlendFunction;
+import technology.sola.engine.graphics.renderer.blend.LightenBlendFunction;
+import technology.sola.engine.graphics.renderer.blend.LinearDodgeBlendFunction;
+import technology.sola.engine.graphics.renderer.blend.MaskBlendFunction;
+import technology.sola.engine.graphics.renderer.blend.MultiplyBlendFunction;
+import technology.sola.engine.graphics.renderer.blend.NoBlendFunction;
+import technology.sola.engine.graphics.renderer.blend.NormalBlendFunction;
+
 /**
- * BlendMode controls how the pixel being drawn (top) and the destination pixel (bottom will be blended together to
- * produce a final color that is drawn.
+ * BlendMode is an enum of several prebuilt {@link BlendFunction}s.
  */
-public enum BlendMode {
+public enum BlendMode implements BlendFunction {
   /**
    * No blending happens. Top pixel is used.
    */
-  NO_BLENDING,
+  NO_BLENDING(new NoBlendFunction()),
 
   /**
    * Transparent if {@code alpha < 255} (bottom pixel used).
    */
-  MASK,
+  MASK(new MaskBlendFunction()),
 
   /**
    * Blends top onto bottom based on alpha of top pixel.
    */
-  NORMAL,
+  NORMAL(new NormalBlendFunction()),
 
   /**
    * Random chance of using color of top pixel based on alpha as the probability. An alpha of 127 would be a 50% chance
    * of using the top pixel.
    */
-  DISSOLVE,
+  DISSOLVE(new DissolveBlendFunction()),
 
   /**
    * Adds the top and bottom pixels together ignoring alpha.
    */
-  LINEAR_DODGE,
+  LINEAR_DODGE(new LinearDodgeBlendFunction()),
 
   /**
    * Takes the RGB channel values from 0 to 1 of each pixel in the top layer and multiples them with the values for the
    * corresponding pixel from the bottom layer.
    */
-  MULTIPLY,
+  MULTIPLY(new MultiplyBlendFunction()),
 
   /**
    * Takes the max value of each channel for the destination pixel; alpha, red, green, blue.
    */
-  LIGHTEN,
+  LIGHTEN(new LightenBlendFunction());
+
+  private final BlendFunction blendFunction;
+
+  BlendMode(BlendFunction blendFunction) {
+    this.blendFunction = blendFunction;
+  }
+
+  @Override
+  public void set(int[] pixels, int pixelIndex, Color color) {
+    blendFunction.set(pixels, pixelIndex, color);
+  }
 }
