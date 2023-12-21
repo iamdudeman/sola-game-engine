c5e82a52463b3993265139e0ac363f6711cedf04
iamdudeman <timothy.solum@gmail.com>
2023-12-20 19:20:16 -0600

    TextGuiElement now loads fonts again

--- sola-engine/src/main/java/technology/sola/engine/graphics/guiv2/elements/TextGuiElement.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/guiv2/elements/TextGuiElement.java
@@ -11,11 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TextGuiElement extends GuiElement<TextStyles> {
-  private Font font = DefaultFont.get();
-  private int lineHeight = 1;
-  private List<String> lines = new ArrayList<>();
   // properties
   private String text;
+
+  // internals
+  private Font font = DefaultFont.get();
+  private String currentFontId = DefaultFont.ASSET_ID;
+  private int lineHeight = 1;
+  private List<String> lines = new ArrayList<>();
 
   public TextGuiElement(TextStyles... styles) {
     super(styles);
@@ -23,13 +26,6 @@ public class TextGuiElement extends GuiElement<TextStyles> {
 
   @Override
   public void renderContent(Renderer renderer) {
-//    if (text != null) {
-//      Color textColor = getStyles().getPropertyValue(TextStyles::textColor, Color.BLACK);
-//
-//      renderer.setFont(font);
-//      renderer.drawString(text, contentBounds.x(), contentBounds.x(), textColor);
-//    }
-
     Color textColor = getStyles().getPropertyValue(TextStyles::textColor, Color.BLACK);
     renderer.setFont(font);
 
@@ -44,82 +40,67 @@ public class TextGuiElement extends GuiElement<TextStyles> {
   public GuiElementBounds calculateBounds(GuiElementBounds boundConstraints) {
     // todo cleanup this bad code
     //   maybe find a way to not need to setContentBounds directly!
-    // todo support multiple line wrapping
-//    String fontAssetId = getStyles().getPropertyValue(TextStyles::fontAssetId, DefaultFont.ASSET_ID);
-//
-//    var fontAssetHandle = getAssetLoaderProvider().get(Font.class).get(fontAssetId);
-//
-//    if (fontAssetHandle.isLoading() || font == null) {
-//      fontAssetHandle.executeWhenLoaded(font -> {
-//        this.font = font;
-//        System.out.println("loaded " + text);
-//        invalidateLayout();
-//      });
-//
-//      return super.calculateBounds(boundConstraints);
-//    } else {
-      if (text == null) {
-        setContentBounds(contentBounds.setDimensions(0, 0));
 
-        return bounds;
+    String fontAssetId = getStyles().getPropertyValue(TextStyles::fontAssetId, DefaultFont.ASSET_ID);
+
+    System.out.println(text + " " + fontAssetId);
+
+    if (!fontAssetId.equals(currentFontId)) {
+      var fontAssetHandle = getAssetLoaderProvider().get(Font.class).get(fontAssetId);
+
+      if (fontAssetHandle.isLoading()) {
+        fontAssetHandle.executeWhenLoaded(font -> {
+          this.font = font;
+          this.currentFontId = fontAssetId;
+          System.out.println("loaded " + font.getFontInfo().fontFamily());
+          getParent().invalidateLayout();
+        });
       } else {
-        setBounds(super.calculateBounds(boundConstraints));
+        font = fontAssetHandle.getAsset();
+      }
+    }
 
-        var textDimensions = font.getDimensionsForText(text);
-        lineHeight = Math.max(textDimensions.height(), 1);
-        int textHeight = lineHeight;
-        int textWidth = Math.max(textDimensions.width(), 1);
+    if (text == null) {
+      setContentBounds(contentBounds.setDimensions(0, 0));
+    } else {
+      setBounds(super.calculateBounds(boundConstraints));
 
-        if (contentBounds.width() < textWidth) {
-          textWidth = contentBounds.width();
-          lines = new ArrayList<>();
-          String[] words = text.split(" ");
-          String sentence = words.length > 0 ? words[0] : " ";
+      var textDimensions = font.getDimensionsForText(text);
+      lineHeight = Math.max(textDimensions.height(), 1);
+      int textHeight = lineHeight;
+      int textWidth = Math.max(textDimensions.width(), 1);
 
-          for (int i = 1; i < words.length; i++) {
-            String word = words[i];
-            String tempSentence = sentence + " " + word;
-            Font.TextDimensions sentenceDimensions = font.getDimensionsForText(tempSentence);
+      if (contentBounds.width() < textWidth) {
+        textWidth = contentBounds.width();
+        lines = new ArrayList<>();
+        String[] words = text.split(" ");
+        String sentence = words.length > 0 ? words[0] : " ";
 
-            if (sentenceDimensions.width() < contentBounds.width()) {
-              sentence = tempSentence;
-            } else {
-              // Start next line with current word
-              lines.add(sentence);
-              sentence = word;
-            }
+        for (int i = 1; i < words.length; i++) {
+          String word = words[i];
+          String tempSentence = sentence + " " + word;
+          Font.TextDimensions sentenceDimensions = font.getDimensionsForText(tempSentence);
+
+          if (sentenceDimensions.width() < contentBounds.width()) {
+            sentence = tempSentence;
+          } else {
+            // Start next line with current word
+            lines.add(sentence);
+            sentence = word;
           }
-
-          lines.add(sentence);
-          textHeight = lineHeight * lines.size();
-        } else {
-          lines = new ArrayList<>(1);
-          lines.add(text);
         }
 
-        setContentBounds(contentBounds.setDimensions(textWidth, textHeight));
-
-        return bounds;
+        lines.add(sentence);
+        textHeight = lineHeight * lines.size();
+      } else {
+        lines = new ArrayList<>(1);
+        lines.add(text);
       }
-//    }
 
-    // todo handle multiple lines and text wrapping
+      setContentBounds(contentBounds.setDimensions(textWidth, textHeight));
 
-//    getAssetLoaderProvider().get(Font.class).get(fontAssetId).executeWhenLoaded(font -> {
-//      this.font = font;
-//
-//      if (text == null) {
-//        // todo this is only true if "align self" was set to "flex-start"
-//        setContentBounds(contentBounds.setDimensions(0, 0));
-//      } else {
-//        var textDimensions = font.getDimensionsForText(text);
-//
-//        // todo this is only true if "align self" was set to "flex-start"
-//        setContentBounds(contentBounds.setDimensions(textDimensions.width(), textDimensions.height()));
-//      }
-//    });
-//
-//    return new GuiElementBounds(0, 0 ,10, 10); // todo real value
+    }
+      return bounds;
   }
 
   public String getText() {
