be73b75e7a10ead42622d4fa70a208852df639eb
iamdudeman <timothy.solum@gmail.com>
2024-01-24 21:01:04 -0600

    Added a few more JavaDocs for files in guiv2 style property

--- sola-engine/src/main/java/technology/sola/engine/graphics/guiv2/style/property/CrossAxisChildren.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/guiv2/style/property/CrossAxisChildren.java
@@ -1,8 +1,24 @@
 package technology.sola.engine.graphics.guiv2.style.property;
 
+/**
+ * CrossAxisChildren defines how a {@link technology.sola.engine.graphics.guiv2.GuiElement}'s children when be positioned
+ * when extra space is available on the cross axis defined by its {@link Direction}.
+ */
 public enum CrossAxisChildren {
+  /**
+   * Children are stretched across the axis.
+   */
   STRETCH,
+  /**
+   * Children are aligned to the start of the axis.
+   */
   START,
+  /**
+   * Children are aligned to the center of the axis.
+   */
   CENTER,
+  /**
+   * Children are aligned to the end of the axis.
+   */
   END,
 }
