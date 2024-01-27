52cc4fcb44520306f22eea1c1ebdf73539aada69
iamdudeman <timothy.solum@gmail.com>
2024-01-27 16:56:48 -0600

    Added more JavaDocs

--- sola-engine/src/main/java/technology/sola/engine/graphics/gui/GuiDocument.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/gui/GuiDocument.java
@@ -8,10 +8,20 @@ import technology.sola.engine.graphics.renderer.Renderer;
 import technology.sola.engine.input.KeyEvent;
 import technology.sola.engine.input.MouseEvent;
 
+/**
+ * GuiDocument is a container for {@link GuiElement} that also handles passing various key and mouse events to the
+ * currently active root element set via {@link GuiDocument#setRootElement(GuiElement)}.
+ */
 public class GuiDocument {
   private final RootGuiElement root;
   private GuiElement<?> focussedElement;
 
+  /**
+   * Creates a new GuiDocument instance, registering listeners for key and mouse related events.
+   *
+   * @param platform            the {@link SolaPlatform}
+   * @param assetLoaderProvider the {@link AssetLoaderProvider}
+   */
   public GuiDocument(SolaPlatform platform, AssetLoaderProvider assetLoaderProvider) {
     root = new RootGuiElement(this, assetLoaderProvider, platform.getRenderer().getWidth(), platform.getRenderer().getHeight());
     focussedElement = root;
@@ -24,6 +34,12 @@ public class GuiDocument {
     platform.onMouseReleased(this::onMouseReleased);
   }
 
+  /**
+   * Sets the current root element that is used for rendering and receiving events to the desired {@link GuiElement}.
+   * Also updates the currently focussed element.
+   *
+   * @param rootEle the new root element
+   */
   public void setRootElement(GuiElement<?> rootEle) {
     var previousElement = this.focussedElement;
 
@@ -41,14 +57,33 @@ public class GuiDocument {
     rootEle.requestFocus();
   }
 
+  /**
+   * Searches the document for a {@link GuiElement} with the desired id.
+   *
+   * @param id           the id of the element
+   * @param elementClass the class of the element
+   * @param <T>          the type of the element
+   * @return the element
+   */
   public <T extends GuiElement<?>> T findElementById(String id, Class<T> elementClass) {
     return root.findElementById(id, elementClass);
   }
 
+  /**
+   * Checks to see if a {@link GuiElement} currently has focus.
+   *
+   * @param guiElement the element to check
+   * @return true if the element is currently focussed
+   */
   public boolean isFocussed(GuiElement<?> guiElement) {
     return this.focussedElement == guiElement;
   }
 
+  /**
+   * Gives the desired {@link GuiElement} focus.
+   *
+   * @param guiElement the new element to be focussed
+   */
   public void requestFocus(GuiElement<?> guiElement) {
     if (guiElement.isFocusable()) {
       var previousElement = this.focussedElement;
@@ -63,6 +98,11 @@ public class GuiDocument {
     }
   }
 
+  /**
+   * Renders the root element to the {@link Renderer}.
+   *
+   * @param renderer the renderer
+   */
   public void render(Renderer renderer) {
     root.render(renderer);
   }
