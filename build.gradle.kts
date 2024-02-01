9389f565248f5e64478bb51e718e6c1a123b9e72
iamdudeman <timothy.solum@gmail.com>
2024-01-31 19:33:57 -0600

    attempt 4 at getting plugin publishing working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
 subprojects {
   apply(plugin = "sola.java-conventions")
   apply(plugin = "maven-publish")
+
+  val publishTask = project.tasks.getByName("publishToMavenLocal")
+
+  gradle.includedBuilds.forEach { build ->
+    publishTask.dependsOn(build.task(":publishToMavenLocal"))
+  }
 }
