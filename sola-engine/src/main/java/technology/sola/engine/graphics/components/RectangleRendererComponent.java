02c67b47d627e7c1e02ce988059dd145796c370b
iamdudeman <timothy.solum@gmail.com>
2024-02-02 10:56:43 -0600

    updated todo list and added JavaDoc to RectangleRendererComponent

--- sola-engine/src/main/java/technology/sola/engine/graphics/components/RectangleRendererComponent.java
+++ sola-engine/src/main/java/technology/sola/engine/graphics/components/RectangleRendererComponent.java
@@ -3,24 +3,62 @@ package technology.sola.engine.graphics.components;
 import technology.sola.ecs.Component;
 import technology.sola.engine.graphics.Color;
 
+/**
+ * RectangleRendererComponent is a {@link Component} containing data for rendering 2d rectangles.
+ */
 public class RectangleRendererComponent implements Component {
-  private final Color color;
-  private final boolean isFilled;
+  private Color color;
+  private boolean isFilled;
 
+  /**
+   * Creates a RectangleRendererComponent of desired color that is filled.
+   *
+   * @param color the {@link Color} of the rectangle
+   */
   public RectangleRendererComponent(Color color) {
     this(color, true);
   }
 
+  /**
+   * Creates a RectangleRendererComponent of desired color that is either filled or not filled.
+   *
+   * @param color    the {@link Color} of the rectangle
+   * @param isFilled whether the circle should be filled or not
+   */
   public RectangleRendererComponent(Color color, boolean isFilled) {
     this.color = color;
     this.isFilled = isFilled;
   }
 
+  /**
+   * @return the {@link Color} of the rectangle
+   */
   public Color getColor() {
     return color;
   }
 
+  /**
+   * @return true if the rectangle should be filled when rendered
+   */
   public boolean isFilled() {
     return isFilled;
+  }
+
+  /**
+   * Sets the {@link Color} for rendering.
+   *
+   * @param color the new color
+   */
+  public void setColor(Color color) {
+    this.color = color;
+  }
+
+  /**
+   * Sets whether the rectangle should be filled or not.
+   *
+   * @param filled the new filled status
+   */
+  public void setFilled(boolean filled) {
+    isFilled = filled;
   }
 }
