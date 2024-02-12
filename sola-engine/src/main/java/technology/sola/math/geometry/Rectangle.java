b9569d6325295aa95d8a4f8f1bd0fe5f6486a48a
iamdudeman <timothy.solum@gmail.com>
2024-02-12 17:18:51 -0600

    added a couple new utility methods to Rectangle

--- sola-engine/src/main/java/technology/sola/math/geometry/Rectangle.java
+++ sola-engine/src/main/java/technology/sola/math/geometry/Rectangle.java
@@ -55,4 +55,34 @@ public record Rectangle(Vector2D min, Vector2D max) {
   public boolean contains(Vector2D point) {
     return point.x() >= min.x() && point.x() <= max.x() && point.y() >= min.y() && point.y() <= max.y();
   }
+
+  /**
+   * Checks to see if this Rectangle intersects with another Rectangle.
+   *
+   * @param rectangle the other Rectangle to check intersection with
+   * @return true if they are intersecting
+   */
+  public boolean intersects(Rectangle rectangle) {
+    Vector2D aBoxMin = min();
+    Vector2D aBoxMax = max();
+    Vector2D bBoxMin = rectangle.min();
+    Vector2D bBoxMax = rectangle.max();
+
+    boolean isOverlappingX = bBoxMin.x() <= aBoxMax.x() && aBoxMin.x() <= bBoxMax.x();
+
+    if (!isOverlappingX) {
+      return false;
+    }
+
+    return bBoxMin.y() <= aBoxMax.y() && aBoxMin.y() <= bBoxMax.y();
+  }
+
+  /**
+   * @return the center point of the rectangle
+   */
+  public Vector2D getCenter() {
+    var firstStep = max.subtract(min);
+
+    return min.add(firstStep.scalar(0.5f));
+  }
 }
