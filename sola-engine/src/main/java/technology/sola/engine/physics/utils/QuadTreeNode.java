c1fd04450b47b0118854023a64a2207e43c016fd
iamdudeman <timothy.solum@gmail.com>
2024-02-12 17:19:09 -0600

    added more implementation details to QuadTreeNode

--- sola-engine/src/main/java/technology/sola/engine/physics/utils/QuadTreeNode.java
+++ sola-engine/src/main/java/technology/sola/engine/physics/utils/QuadTreeNode.java
@@ -1,17 +1,20 @@
 package technology.sola.engine.physics.utils;
 
-import technology.sola.ecs.Entity;
+import technology.sola.ecs.view.View2Entry;
+import technology.sola.engine.core.component.TransformComponent;
+import technology.sola.engine.physics.component.ColliderComponent;
 import technology.sola.math.geometry.Rectangle;
+import technology.sola.math.linear.Vector2D;
 
 import java.util.*;
 
 public class QuadTreeNode {
-  private static int maxDepth = 5;
-  private static int maxObjectsPerNode = 15;
+  private static final int MAX_DEPTH = 5;
+  private static final int MAX_ENTITIES_PER_NODE = 15;
 
-  private Rectangle nodeBounds;
-  private List<QuadTreeNode> children = new ArrayList<>();
-  private List<QuadTreeData> contents = new ArrayList<>();
+  private final Rectangle nodeBounds;
+  private final List<QuadTreeNode> children = new ArrayList<>();
+  private final List<QuadTreeData> contents = new ArrayList<>();
   private int currentDepth;
 
   public QuadTreeNode(Rectangle nodeBounds) {
@@ -62,41 +65,166 @@ public class QuadTreeNode {
   }
 
   public void insert(QuadTreeData data) {
-    throw new RuntimeException();
+    if (!data.entityBoundingRectangle.intersects(nodeBounds)) {
+      return;
+    }
+
+    if (isLeaf()) {
+      if (contents.size() + 1 > MAX_ENTITIES_PER_NODE) {
+        split();
+      }
+
+      contents.add(data);
+    } else {
+      for (int i = 0, size = children.size(); i < size; i++) {
+        children.get(i).insert(data);
+      }
+    }
   }
 
   public void remove(QuadTreeData data) {
-    throw new RuntimeException();
+    if (isLeaf()) {
+      int removeIndex = -1;
+
+      for (int i = 0, size = contents.size(); i < size; i++) {
+        if (contents.get(0).entityView.entity() == data.entityView.entity()) {
+          removeIndex = i;
+          break;
+        }
+      }
+
+      if (removeIndex != -1) {
+        contents.remove(removeIndex);
+      }
+    } else {
+      for (int i = 0, size = children.size(); i < size; i++) {
+        children.get(i).remove(data);
+      }
+    }
+
+    shake();
   }
 
   public void update(QuadTreeData data) {
-    throw new RuntimeException();
+    remove(data);
+    insert(data);
   }
 
   public void shake() {
-    throw new RuntimeException();
+    if (!isLeaf()) {
+      int entityCount = entityCount();
+
+      if (entityCount == 0) {
+        children.clear();
+      } else if (entityCount < MAX_ENTITIES_PER_NODE) {
+        Deque<QuadTreeNode> process = new ArrayDeque<>();
+
+        process.push(this);
+
+        while (!process.isEmpty()) {
+          var processing = process.getLast();
+
+          if (!processing.isLeaf()) {
+            for (var child : children) {
+              process.push(child);
+            }
+          } else {
+            contents.addAll(processing.contents);
+          }
+
+          process.remove();
+        }
+
+        children.clear();
+      }
+    }
   }
 
   public void split() {
-    throw new RuntimeException();
+    if (currentDepth + 1 >= MAX_DEPTH) {
+      return;
+    }
+
+    var min = nodeBounds.min();
+    var max = nodeBounds.max();
+    var center = nodeBounds.getCenter();
+
+    Rectangle[] childAreas = {
+      new Rectangle(min, center),
+      new Rectangle(new Vector2D(center.x(), min.y()), new Vector2D(max.x(), center.y())),
+      new Rectangle(center, max),
+      new Rectangle(new Vector2D(min.x(), center.y()), new Vector2D(center.x(), max.y())),
+    };
+
+    for (int i = 0; i < 4; i++) {
+      children.add(new QuadTreeNode(childAreas[i]));
+      children.get(i).currentDepth = currentDepth + 1;
+    }
+
+    for (int i = 0, size = contents.size(); i < size; i++) {
+      children.get(i).insert(contents.get(i));
+    }
+
+    contents.clear();
   }
 
   public void reset() {
-    throw new RuntimeException();
+    if (isLeaf()) {
+      for (var content : contents) {
+        content.flag = false;
+      }
+    } else {
+      for (var child : children) {
+        child.reset();
+      }
+    }
   }
 
   public List<QuadTreeData> query(Rectangle area) {
-    throw new RuntimeException();
+    List<QuadTreeData> result = new ArrayList<>();
+
+    if (!area.intersects(nodeBounds)) {
+      return result;
+    }
+
+    if (isLeaf()) {
+      for (int i = 0, size = contents.size(); i < size; i++) {
+        if (contents.get(i).entityBoundingRectangle.intersects(area)) {
+          result.add(contents.get(i));
+        }
+      }
+    } else {
+      for (int i = 0, size = children.size(); i < size; i++) {
+        result.addAll(children.get(i).query(area));
+      }
+    }
+
+    return result;
   }
 
-  public class QuadTreeData {
-    private Entity entity;
-    private Rectangle entityBoundingRectangle;
+  public static class QuadTreeData {
+    private final View2Entry<ColliderComponent, TransformComponent> entityView;
+    private final Rectangle entityBoundingRectangle;
     private boolean flag;
 
-    public QuadTreeData(Entity entity, Rectangle entityBoundingRectangle) {
-      this.entity = entity;
+    private QuadTreeData(View2Entry<ColliderComponent, TransformComponent> entityView, Rectangle entityBoundingRectangle) {
+      this.entityView = entityView;
       this.entityBoundingRectangle = entityBoundingRectangle;
+    }
+
+    public QuadTreeData fromView(View2Entry<ColliderComponent, TransformComponent> view) {
+      var transformComponent = view.c2();
+      var colliderComponent = view.c1();
+      var boundingRectangle = new Rectangle(
+        transformComponent.getTranslate()
+          .add(new Vector2D(colliderComponent.getOffsetX(), colliderComponent.getOffsetY())),
+        new Vector2D(
+          colliderComponent.getBoundingWidth(transformComponent.getScaleX()),
+          colliderComponent.getBoundingHeight(transformComponent.getScaleY())
+        )
+      );
+
+      return new QuadTreeData(view, boundingRectangle);
     }
   }
 }
