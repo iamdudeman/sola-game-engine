869818b3722ecd26c5dc82285ba22536fc58d666
iamdudeman <timothy.solum@gmail.com>
2024-04-13 21:58:08 -0500

    rough code for SolaInput implementation

--- sola-engine/src/main/java/technology/sola/engine/input/SolaInput.java
+++ sola-engine/src/main/java/technology/sola/engine/input/SolaInput.java
@@ -1,10 +1,11 @@
 package technology.sola.engine.input;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 /*
 todo
@@ -12,53 +13,98 @@ this could be populated by a controls.input.json file or something like that
 [
   {
     "id": "jump",
-    "controls": [
-      [{ "type": "key", "key": "A" }, { "type": "key", "key": "SHIFT" }]
+    "inputs": [
+      [{ "type": "key", "key": "A", "state": "PRESSED" }, { "type": "key", "key": "SHIFT", "state": "PRESSED" }]
     ]
   },
   {
     "id": "jump",
-    "controls": [
-      { "type": "mouse", "button": "PRIMARY" }
+    "inputs": [
+      { "type": "mouse", "button": "PRIMARY", "state": "PRESSED" }
     ]
   }
 ]
  */
 
 public class SolaInput {
+  private static final Logger LOGGER = LoggerFactory.getLogger(SolaInput.class);
+  private final Map<String, Control> controlMap = new HashMap<>();
+  private final KeyboardInput keyboardInput;
+  private final MouseInput mouseInput;
 
   public SolaInput(KeyboardInput keyboardInput, MouseInput mouseInput) {
-    // todo
+    this.keyboardInput = keyboardInput;
+    this.mouseInput = mouseInput;
   }
 
-  public boolean isOn(String inputId) {
-    throw new RuntimeException("not yet implemented");
+  public boolean isActive(String controlId) {
+    var control = controlMap.get(controlId);
+
+    if (control == null) {
+      LOGGER.warn("Control with id {} not found", controlId);
+      return false;
+    }
+
+    for (var inputs : control.inputs) {
+      boolean isInputActive = true;
+
+      for (var input : inputs) {
+        if (!input.isActive(keyboardInput, mouseInput)) {
+          isInputActive = false;
+          break;
+        }
+      }
+
+      if (isInputActive) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
-  public void add(Input input) {
-    throw new RuntimeException("not yet implemented");
+  public void setControl(String id, Control control) {
+    controlMap.put(id, control);
   }
 
-  public record Input(
-    String id, List<List<Control>> controls
-  ) {
+  public record Control(String id, List<List<Input<?>>> inputs) {
   }
 
-  interface Control {
-    String type();
+  interface Input<T> {
+    T state();
+
+    boolean isActive(KeyboardInput keyboardInput, MouseInput mouseInput);
   }
 
-  public record KeyControl(Key key) implements Control {
+  public record KeyInput(Key key, KeyState state) implements Input<KeyState> {
     @Override
-    public String type() {
-      return "key";
+    public boolean isActive(KeyboardInput keyboardInput, MouseInput mouseInput) {
+      if (state == KeyState.PRESSED) {
+        return keyboardInput.isKeyPressed(key);
+      } else {
+        return keyboardInput.isKeyHeld(key);
+      }
     }
   }
 
-  public record MouseControl(MouseButton button) implements Control {
+  public enum KeyState {
+    PRESSED,
+    HELD
+  }
+
+  public record MouseButtonInput(MouseButton button, MouseState state) implements Input<MouseState> {
     @Override
-    public String type() {
-      return "mouse";
+    public boolean isActive(KeyboardInput keyboardInput, MouseInput mouseInput) {
+      if (state == MouseState.PRESSED) {
+        return mouseInput.isMousePressed(button);
+      } else {
+        return mouseInput.isMouseDragged(button);
+      }
     }
+  }
+
+  public enum MouseState {
+    PRESSED,
+    DRAGGED
   }
 }
