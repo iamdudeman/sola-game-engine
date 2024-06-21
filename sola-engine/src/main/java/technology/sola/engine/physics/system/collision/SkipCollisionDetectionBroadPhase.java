65c348b52d228c0e280e4e0bf802e6a8b50e46ad
Timothy Solum <timothy.solum@gmail.com>
2024-06-21 14:58:40 -0500

    Implemented SkipCollisionDetectionBroadPhase for games where a broad phase isn't needed (#172)

---
+++ sola-engine/src/main/java/technology/sola/engine/physics/system/collision/SkipCollisionDetectionBroadPhase.java
@@ -0,0 +1,32 @@
+package technology.sola.engine.physics.system.collision;
+
+import technology.sola.ecs.view.View2Entry;
+import technology.sola.engine.core.component.TransformComponent;
+import technology.sola.engine.graphics.renderer.Renderer;
+import technology.sola.engine.physics.component.ColliderComponent;
+import technology.sola.math.linear.Matrix3D;
+
+import java.util.List;
+
+/**
+ * SkipCollisionDetectionBroadPhase is a {@link CollisionDetectionBroadPhase} implementation that skips the broad phase
+ * check if a game does not require it.
+ */
+public class SkipCollisionDetectionBroadPhase implements CollisionDetectionBroadPhase {
+  private List<View2Entry<ColliderComponent, TransformComponent>> views;
+
+  @Override
+  public void populate(List<View2Entry<ColliderComponent, TransformComponent>> views) {
+    this.views = views;
+  }
+
+  @Override
+  public List<View2Entry<ColliderComponent, TransformComponent>> query(View2Entry<ColliderComponent, TransformComponent> searchEntry) {
+    return views;
+  }
+
+  @Override
+  public void renderDebug(Renderer renderer, Matrix3D cameraScaleTransform, Matrix3D cameraTranslationTransform) {
+    // Nothing needed here
+  }
+}
