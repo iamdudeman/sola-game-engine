dab85920fc3f4be9a4b2b2cac153c27fbc04e242
iamdudeman <timothy.solum@gmail.com>
2024-10-03 17:56:35 -0500

    fixed typos

--- sola-engine/editor/src/main/java/module-info.java
+++ sola-engine/editor/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 /**
  * Defines the sola-game-engine Editor API.
  */
-module technology.sola.engine.edtiro {
+module technology.sola.engine.editor {
   requires org.slf4j;
   requires transitive technology.sola.engine;
 
