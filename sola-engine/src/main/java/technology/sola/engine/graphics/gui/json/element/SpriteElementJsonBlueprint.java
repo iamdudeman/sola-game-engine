6bfac3e42f437726ecc2f0952d5015e65f694cbe
Timothy Solum <timothy.solum@gmail.com>
2025-02-22 08:46:50 -0600

    Feature/spritesheet gui element (#193)
    * added SpriteGuiElement implementation
    
    * added SpriteElementJsonBlueprint
    
    * added example of SpriteGuiElement rendering in GuiExample
    
    * bumped version

---
+++ sola-engine/src/main/java/technology/sola/engine/graphics/gui/json/element/SpriteElementJsonBlueprint.java
@@ -0,0 +1,35 @@
+package technology.sola.engine.graphics.gui.json.element;
+
+import technology.sola.engine.graphics.gui.elements.SpriteGuiElement;
+import technology.sola.engine.graphics.gui.json.styles.BaseStylesJsonValueParser;
+import technology.sola.engine.graphics.gui.style.BaseStyles;
+import technology.sola.json.JsonObject;
+
+/**
+ * A {@link GuiElementJsonBlueprint} for {@link SpriteGuiElement}.
+ */
+public class SpriteElementJsonBlueprint extends GuiElementJsonBlueprint<BaseStyles, SpriteGuiElement, BaseStyles.Builder<?>> {
+  /**
+   * Creates an instance of this {@link GuiElementJsonBlueprint}.
+   */
+  public SpriteElementJsonBlueprint() {
+    super(new BaseStylesJsonValueParser());
+  }
+
+  @Override
+  public String getTag() {
+    return "Sprite";
+  }
+
+  @Override
+  public SpriteGuiElement createElementFromJson(JsonObject propsJson) {
+    return new SpriteGuiElement()
+      .setAssetId(propsJson.getString("assetId", null))
+      .setSpriteId(propsJson.getString("spriteId", null));
+  }
+
+  @Override
+  protected BaseStyles.Builder<?> createStylesBuilder() {
+    return BaseStyles.create();
+  }
+}
