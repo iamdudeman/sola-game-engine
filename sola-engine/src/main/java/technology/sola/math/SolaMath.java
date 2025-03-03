9fe62f46040106013ca2a03c0c1d84b34b37d217
Timothy Solum <timothy.solum@gmail.com>
2025-03-03 16:24:28 -0600

    Cleanup/easing function (#195)
    * moved lerping functionality to SolaMath and simplified EasingFunction
    
    * updated some JavaDocs
    
    * added methods to lerp vectors

--- sola-engine/src/main/java/technology/sola/math/SolaMath.java
+++ sola-engine/src/main/java/technology/sola/math/SolaMath.java
@@ -7,6 +7,63 @@ import technology.sola.math.linear.Vector2D;
  */
 public final class SolaMath {
   /**
+   * Linearly interpolates a value between start and end.
+   *
+   * @param start   the minimum value to return
+   * @param end     the maximum value to return
+   * @param percent the "progress" towards the end value
+   * @return the linearly interpolated value clamped between start and end
+   */
+  public static float lerp(float start, float end, float percent) {
+    return lerp(start, end, percent, EasingFunction.LINEAR);
+  }
+
+  /**
+   * Interpolates a value between start and end via desired {@link EasingFunction}.
+   *
+   * @param start          the minimum value to return
+   * @param end            the maximum value to return
+   * @param percent        the "progress" towards the end value
+   * @param easingFunction the {@link EasingFunction} used during interpolation
+   * @return the interpolated value clamped between start and end
+   */
+  public static float lerp(float start, float end, float percent, EasingFunction easingFunction) {
+    if (start == end) {
+      return start;
+    }
+
+    return start + (end - start) * easingFunction.ease(percent);
+  }
+
+  /**
+   * Linearly interpolates a {@link Vector2D} value between start and end.
+   *
+   * @param start   the minimum value to return
+   * @param end     the maximum value to return
+   * @param percent the "progress" towards the end value
+   * @return the linearly interpolated vector clamped between start and end
+   */
+  public static Vector2D lerp(Vector2D start, Vector2D end, float percent) {
+    return lerp(start, end, percent, EasingFunction.LINEAR);
+  }
+
+  /**
+   * Interpolates a {@link Vector2D} value between start and end via desired {@link EasingFunction}.
+   *
+   * @param start          the minimum value to return
+   * @param end            the maximum value to return
+   * @param percent        the "progress" towards the end value
+   * @param easingFunction the {@link EasingFunction} used during interpolation
+   * @return the interpolated vector clamped between start and end
+   */
+  public static Vector2D lerp(Vector2D start, Vector2D end, float percent, EasingFunction easingFunction) {
+    var x = lerp(start.x(), end.x(), percent, easingFunction);
+    var y = lerp(start.y(), end.y(), percent, easingFunction);
+
+    return new Vector2D(x, y);
+  }
+
+  /**
    * Clamps a float value between a min and max.
    *
    * @param min   the minimum value
