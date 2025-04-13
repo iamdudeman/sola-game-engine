0287f568559bbf330330f85102dc57117c3b09bd
iamdudeman <timothy.solum@gmail.com>
2025-04-13 17:32:02 -0500

    now updating font json image after rename

--- sola-engine/editor/src/main/java/technology/sola/engine/editor/font/FontLeftPanel.java
+++ sola-engine/editor/src/main/java/technology/sola/engine/editor/font/FontLeftPanel.java
@@ -1,12 +1,20 @@
 package technology.sola.engine.editor.font;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import technology.sola.engine.editor.core.components.AssetTreeView;
 import technology.sola.engine.editor.core.components.EditorPanel;
 import technology.sola.engine.editor.core.components.TabbedPanel;
+import technology.sola.engine.editor.core.notifications.Toast;
+import technology.sola.json.SolaJson;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class FontLeftPanel extends EditorPanel {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FontLeftPanel.class);
+
   public FontLeftPanel(TabbedPanel centerPanel) {
     super();
 
@@ -50,9 +58,21 @@ public class FontLeftPanel extends EditorPanel {
       var imageAsset = new File(parentFile, oldItem.label() + ".png");
       var newImageAsset = newItem.file().getName().replace(".font.json", "") + ".png";
 
-      imageAsset.renameTo(new File(parentFile, newImageAsset));
+      try {
+        var jsonFileString = Files.readString(newItem.file().toPath());
+        var jsonObject = new SolaJson().parse(jsonFileString).asObject();
 
-      centerPanel.renameTab(oldItem.id(), newItem.label(), newItem.id());
+        jsonObject.put("fontGlyphFile", newImageAsset);
+
+        if (imageAsset.renameTo(new File(parentFile, newImageAsset))) {
+          Files.writeString(newItem.file().toPath(), jsonObject.toString());
+
+          centerPanel.renameTab(oldItem.id(), newItem.label(), newItem.id());
+        }
+      } catch (IOException ex) {
+        Toast.error(ex.getMessage());
+        LOGGER.error(ex.getMessage(), ex);
+      }
     }
 
     @Override
