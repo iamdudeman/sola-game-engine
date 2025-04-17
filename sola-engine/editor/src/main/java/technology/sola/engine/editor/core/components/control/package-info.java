ab42e76b20eb7bf5a43ae8ab8eb95d5850013d83
iamdudeman <timothy.solum@gmail.com>
2025-04-17 17:07:22 -0500

    added more missing JavaDocs

--- sola-engine/editor/src/main/java/technology/sola/engine/editor/core/components/control/package-info.java
+++ sola-engine/editor/src/main/java/technology/sola/engine/editor/core/components/control/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Package containing control ui components for the editor.
+ * Package containing ui components the user inputs values with for the editor.
  */
 package technology.sola.engine.editor.core.components.control;
