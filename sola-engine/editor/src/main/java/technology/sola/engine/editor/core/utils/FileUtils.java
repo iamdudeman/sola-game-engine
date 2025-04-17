6e0f3fb5db78ea9d08077b4f6bf6e9ca6bb6f042
iamdudeman <timothy.solum@gmail.com>
2025-04-17 17:02:14 -0500

    added more missing JavaDocs

--- sola-engine/editor/src/main/java/technology/sola/engine/editor/core/utils/FileUtils.java
+++ sola-engine/editor/src/main/java/technology/sola/engine/editor/core/utils/FileUtils.java
@@ -8,21 +8,52 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+/**
+ * FileUtils contains utility methods for reading and writing file content.
+ */
 public class FileUtils {
+  /**
+   * Reads a {@link JsonElement} from desired {@link File}.
+   *
+   * @param file the {@link File} to read json from
+   * @return the parsed {@link JsonElement} from the file
+   * @throws IOException if an I/O error occurs reading from the file or a malformed or unmappable byte sequence is read
+   */
   public static JsonElement readJson(File file) throws IOException {
     var jsonFileString = Files.readString(file.toPath());
 
     return new SolaJson().parse(jsonFileString);
   }
 
+  /**
+   * Writes a {@link JsonElement} to desired {@link File}.
+   *
+   * @param file    the {@link File} to write json to
+   * @param element the {@link JsonElement} to write to the file
+   * @throws IOException if an I/O error occurs reading from the file or a malformed or unmappable byte sequence is read
+   */
   public static void writeJson(File file, JsonElement element) throws IOException {
     Files.writeString(file.toPath(), element.toString());
   }
 
+  /**
+   * Writes a {@link JsonObject} to desired {@link File}.
+   *
+   * @param file   the {@link File} to write json to
+   * @param object the {@link JsonObject} to write to the file
+   * @throws IOException if an I/O error occurs reading from the file or a malformed or unmappable byte sequence is read
+   */
   public static void writeJson(File file, JsonObject object) throws IOException {
     Files.writeString(file.toPath(), object.toString());
   }
 
+  /**
+   * Writes a {@link JsonObject} to desired {@link File} with pretty printing.
+   *
+   * @param file   the {@link File} to write json to
+   * @param object the {@link JsonObject} to write to the file
+   * @throws IOException if an I/O error occurs reading from the file or a malformed or unmappable byte sequence is read
+   */
   public static void writePrettyJson(File file, JsonObject object) throws IOException {
     Files.writeString(file.toPath(), object.toString(2));
   }
---
+++ sola-engine/editor/src/main/java/technology/sola/engine/editor/core/utils/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains various utilities used by the editor.
+ */
+package technology.sola.engine.editor.core.utils;
