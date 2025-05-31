6c91dc7a24fb65ae481c5b6f940e0faa3c87c5d0
iamdudeman <timothy.solum@gmail.com>
2025-05-30 21:49:17 -0500

    started splitting particle emitter properties into separate classes

---
+++ sola-engine/src/main/java/technology/sola/engine/physics/component/particle/ParticleAppearance.java
@@ -0,0 +1,14 @@
+package technology.sola.engine.physics.component.particle;
+
+import technology.sola.engine.graphics.Color;
+
+public class ParticleAppearance {
+  private float particleMinSize = 8f;
+  private float particleMaxSize = 8f;
+
+  // todo consider size at start of lifetime option
+  // todo consider size at end of lifetime option
+
+  private Color particleColor = Color.WHITE;
+
+}
---
+++ sola-engine/src/main/java/technology/sola/engine/physics/component/particle/ParticleMovement.java
@@ -0,0 +1,10 @@
+package technology.sola.engine.physics.component.particle;
+
+import technology.sola.math.linear.Vector2D;
+
+public class ParticleMovement {
+  private Vector2D particleMinVelocity = new Vector2D(-50, -100);
+  private Vector2D particleMaxVelocity = new Vector2D(50, -0.1f);
+
+  // todo investigate the concept of "noise" in movement
+}
