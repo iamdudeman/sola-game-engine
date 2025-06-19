6cf86a88efb3bf2ce66ae0e589be27a2d2f08ca5
Timothy Solum <timothy.solum@gmail.com>
2025-06-19 16:36:08 -0500

    Refactored local Java conventions plugin into a distributed plugin (#212)
    * introduced new SolaJavaConventionsPlugin and updated examples to use it
    
    * tooling now uses new sola java conventions plugin
    
    * remove need for sola.platform-conventions.gradle.kts plugin
    
    * technology.sola.plugins.sola-java-conventions now used everywhere
    
    * removed local java-conventions plugin

--- sola-engine/platform/browser/build.gradle.kts
+++ sola-engine/platform/browser/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
-  id("sola.platform-conventions")
+  id("technology.sola.plugins.sola-java-conventions")
   id("technology.sola.sola-publishing")
 }
 
 val teaVmVersion = "0.10.0"
 
 dependencies {
+  api(project(":sola-engine"))
+
   implementation("org.teavm:teavm-tooling:$teaVmVersion")
   implementation("org.teavm:teavm-classlib:$teaVmVersion")
 }
