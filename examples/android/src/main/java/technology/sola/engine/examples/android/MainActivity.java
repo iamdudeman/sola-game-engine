20a20644dea0afd27ba2a51941af005e57e1eed4
iamdudeman <timothy.solum@gmail.com>
2025-06-19 10:13:43 -0500

    prep for hooking up android platform module

--- examples/android/src/main/java/technology/sola/engine/examples/android/MainActivity.java
+++ examples/android/src/main/java/technology/sola/engine/examples/android/MainActivity.java
@@ -1,4 +1,12 @@
 package technology.sola.engine.examples.android;
 
+import technology.sola.logging.JavaSolaLoggerFactory;
+import technology.sola.logging.SolaLogLevel;
+import technology.sola.logging.SolaLogger;
+
 public class MainActivity extends TempActivity {
+  static {
+    // todo might need AndroidSolaLoggerFactory
+    SolaLogger.configure(SolaLogLevel.WARNING, new JavaSolaLoggerFactory());
+  }
 }
--- sola-engine/platform/android/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Defines the sola-game-engine Android platform API.
- */
-module technology.sola.engine.platform.android {
-  requires java.desktop;
-  requires transitive technology.sola.engine;
-
-  exports technology.sola.engine.platform.android;
-}
