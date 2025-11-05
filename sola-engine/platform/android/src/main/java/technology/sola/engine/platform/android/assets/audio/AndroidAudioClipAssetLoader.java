066d4533d8c5f187495b492f85df9a68b8587a9a
iamdudeman <timothy.solum@gmail.com>
2025-11-05 16:44:39 -0600

    more code cleanup

--- sola-engine/platform/android/src/main/java/technology/sola/engine/platform/android/assets/audio/AndroidAudioClipAssetLoader.java
+++ sola-engine/platform/android/src/main/java/technology/sola/engine/platform/android/assets/audio/AndroidAudioClipAssetLoader.java
@@ -3,9 +3,9 @@ package technology.sola.engine.platform.android.assets.audio;
 import android.content.res.AssetManager;
 import android.media.MediaPlayer;
 import org.jspecify.annotations.NullMarked;
+import technology.sola.engine.assets.AssetExtension;
 import technology.sola.engine.assets.AssetHandle;
 import technology.sola.engine.assets.AssetLoader;
-import technology.sola.engine.assets.AssetUtils;
 import technology.sola.engine.assets.audio.AudioClip;
 import technology.sola.engine.assets.audio.AudioClipException;
 import technology.sola.engine.platform.android.assets.AndroidAssetUtils;
@@ -35,27 +35,23 @@ public class AndroidAudioClipAssetLoader extends AssetLoader<AudioClip> {
 
   @Override
   protected AssetHandle<AudioClip> loadAsset(String path) {
-    var extension = AssetUtils.getExtension(path);
+    AssetExtension.assertPathExtension(path, AssetExtension.MP3, AssetExtension.WAV);
 
-    if (".wav".equals(extension)) {
-      AssetHandle<AudioClip> audioClipAssetHandle = new AssetHandle<>();
+    AssetHandle<AudioClip> audioClipAssetHandle = new AssetHandle<>();
 
-      new Thread(() -> {
-        try (var assetFileDescriptor = assetManager.openFd(AndroidAssetUtils.sanitizeAssetPath(path))) {
-          MediaPlayer mediaPlayer = new MediaPlayer();
+    new Thread(() -> {
+      try (var assetFileDescriptor = assetManager.openFd(AndroidAssetUtils.sanitizeAssetPath(path))) {
+        MediaPlayer mediaPlayer = new MediaPlayer();
 
-          mediaPlayer.setDataSource(assetFileDescriptor);
-          mediaPlayer.prepare();
+        mediaPlayer.setDataSource(assetFileDescriptor);
+        mediaPlayer.prepare();
 
-          audioClipAssetHandle.setAsset(new AndroidWavAudioClip(mediaPlayer));
-        } catch (IOException ex) {
-          throw new AudioClipException("Could not create AudioClip: " + ex.getMessage());
-        }
-      }).start();
+        audioClipAssetHandle.setAsset(new AndroidWavAudioClip(mediaPlayer));
+      } catch (IOException ex) {
+        throw new AudioClipException("Could not create AudioClip: " + ex.getMessage());
+      }
+    }).start();
 
-      return audioClipAssetHandle;
-    }
-
-    throw new AudioClipException("Could not load AudioClip from file " + path);
+    return audioClipAssetHandle;
   }
 }
--- sola-engine/platform/javafx/src/main/java/technology/sola/engine/platform/javafx/assets/audio/JavaFxAudioClipAssetLoader.java
+++ sola-engine/platform/javafx/src/main/java/technology/sola/engine/platform/javafx/assets/audio/JavaFxAudioClipAssetLoader.java
@@ -1,9 +1,9 @@
 package technology.sola.engine.platform.javafx.assets.audio;
 
 import org.jspecify.annotations.NullMarked;
+import technology.sola.engine.assets.AssetExtension;
 import technology.sola.engine.assets.AssetHandle;
 import technology.sola.engine.assets.AssetLoader;
-import technology.sola.engine.assets.AssetUtils;
 import technology.sola.engine.assets.audio.AudioClip;
 import technology.sola.engine.assets.audio.AudioClipException;
 import technology.sola.engine.platform.javafx.assets.JavaFxPathUtils;
@@ -26,22 +26,18 @@ public class JavaFxAudioClipAssetLoader extends AssetLoader<AudioClip> {
 
   @Override
   protected AssetHandle<AudioClip> loadAsset(String path) {
-    String fileExtension = AssetUtils.getExtension(path);
+    AssetExtension.assertPathExtension(path, AssetExtension.MP3, AssetExtension.WAV);
 
-    if (".wav".equals(fileExtension)) {
-      AssetHandle<AudioClip> audioClipAssetHandle = new AssetHandle<>();
+    AssetHandle<AudioClip> audioClipAssetHandle = new AssetHandle<>();
 
-      new Thread(() -> {
-        try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(JavaFxPathUtils.asUrl(path))) {
-          audioClipAssetHandle.setAsset(new WavAudioClip(AudioSystem.getClip(), audioInputStream));
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
-          throw new AudioClipException("Could not create AudioClip: " + ex.getMessage());
-        }
-      }).start();
+    new Thread(() -> {
+      try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(JavaFxPathUtils.asUrl(path))) {
+        audioClipAssetHandle.setAsset(new WavAudioClip(AudioSystem.getClip(), audioInputStream));
+      } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+        throw new AudioClipException("Could not create AudioClip: " + ex.getMessage());
+      }
+    }).start();
 
-      return audioClipAssetHandle;
-    }
-
-    throw new AudioClipException("Could not load AudioClip from file " + path);
+    return audioClipAssetHandle;
   }
 }
