5eb3d0c705adcea956bb7a9e9a971a95a6d3ed76
iamdudeman <timothy.solum@gmail.com>
2025-11-27 10:04:28 -0600

    code cleanup

--- sola-engine/src/main/java/technology/sola/engine/input/TouchGestureHelper.java
+++ sola-engine/src/main/java/technology/sola/engine/input/TouchGestureHelper.java
@@ -1,5 +1,6 @@
 package technology.sola.engine.input;
 
+import org.jspecify.annotations.Nullable;
 import technology.sola.math.linear.Vector2D;
 
 /**
@@ -19,54 +20,14 @@ public class TouchGestureHelper {
    * @return true if a pinch in gesture has been detected
    */
   public boolean isPinchIn() {
-    Touch touch1 = touchInput.getTouch(0);
-    Touch touch2 = touchInput.getTouch(1);
-
-    if (initialTouch1 == null || initialTouch2 == null || touch1 == null || touch2 == null) {
-      return false;
-    }
-
-    if (touch1.phase() != TouchPhase.MOVED || touch2.phase() != TouchPhase.MOVED) {
-      return false;
-    }
-
-    var currentDistance = new Vector2D(touch2.x(), touch2.y()).distanceSq(new Vector2D(touch1.x(), touch1.y()));
-    var initialDistance = new Vector2D(initialTouch2.x(), initialTouch2.y()).distanceSq(new Vector2D(initialTouch1.x(), initialTouch1.y()));
-
-    if (initialDistance - currentDistance > PINCH_THRESHOLD) {
-      initialTouch1 = touch1;
-      initialTouch2 = touch2;
-      return true;
-    }
-
-    return false;
+    return isPinchGesture(true);
   }
 
   /**
    * @return true if a pinch out gesture has been detected
    */
   public boolean isPinchOut() {
-    Touch touch1 = touchInput.getTouch(0);
-    Touch touch2 = touchInput.getTouch(1);
-
-    if (initialTouch1 == null || initialTouch2 == null || touch1 == null || touch2 == null ) {
-      return false;
-    }
-
-    if (touch1.phase() != TouchPhase.MOVED || touch2.phase() != TouchPhase.MOVED) {
-      return false;
-    }
-
-    var currentDistance = new Vector2D(touch2.x(), touch2.y()).distanceSq(new Vector2D(touch1.x(), touch1.y()));
-    var initialDistance = new Vector2D(initialTouch2.x(), initialTouch2.y()).distanceSq(new Vector2D(initialTouch1.x(), initialTouch1.y()));
-
-    if (currentDistance - initialDistance > PINCH_THRESHOLD) {
-      initialTouch1 = touch1;
-      initialTouch2 = touch2;
-      return true;
-    }
-
-    return false;
+    return isPinchGesture(false);
   }
 
   void update() {
@@ -89,5 +50,32 @@ public class TouchGestureHelper {
         initialTouch2 = temp;
       }
     }
+  }
+
+  private boolean isPinchGesture(boolean isPinchIn) {
+    Touch touch1 = touchInput.getTouch(0);
+    Touch touch2 = touchInput.getTouch(1);
+
+    if (initialTouch1 == null || initialTouch2 == null || touch1 == null || touch2 == null ) {
+      return false;
+    }
+
+    if (touch1.phase() != TouchPhase.MOVED || touch2.phase() != TouchPhase.MOVED) {
+      return false;
+    }
+
+    var currentDistance = new Vector2D(touch2.x(), touch2.y()).distanceSq(new Vector2D(touch1.x(), touch1.y()));
+    var initialDistance = new Vector2D(initialTouch2.x(), initialTouch2.y()).distanceSq(new Vector2D(initialTouch1.x(), initialTouch1.y()));
+
+    float distanceBig = isPinchIn ? initialDistance : currentDistance;
+    float distanceSmall = isPinchIn ? currentDistance : initialDistance;
+
+    if (distanceBig - distanceSmall > PINCH_THRESHOLD) {
+      initialTouch1 = touch1;
+      initialTouch2 = touch2;
+      return true;
+    }
+
+    return false;
   }
 }
