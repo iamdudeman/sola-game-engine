3e24bd5c87e7db45ff6fad27c95055ce9035c178
iamdudeman <timothy.solum@gmail.com>
2025-12-11 17:50:53 -0600

    editor example no longer uses java distribution plugin

--- examples/editor/build.gradle.kts
+++ examples/editor/build.gradle.kts
@@ -1,14 +1,9 @@
 plugins {
   id("technology.sola.plugins.sola-java-conventions")
   id("technology.sola.plugins.sola-javafx")
-  id("technology.sola.plugins.sola-java-distribution")
 }
 
 dependencies {
   implementation(project(":sola-engine:editor"))
   implementation(project(":examples:common"))
-}
-
-solaJavaDist {
-  mainClass = "${project.properties["basePackage"]}.${project.name}.EditorMain"
 }
