bd93a6b272aa785274c79fb63ebb905703e929b1
iamdudeman <timothy.solum@gmail.com>
2026-01-03 09:31:48 -0600

    AudioPlayerPanel now plays audio

--- sola-engine/editor/src/main/java/technology/sola/engine/editor/tools/audio/AudioPlayerPanel.java
+++ sola-engine/editor/src/main/java/technology/sola/engine/editor/tools/audio/AudioPlayerPanel.java
@@ -1,14 +1,65 @@
 package technology.sola.engine.editor.tools.audio;
 
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+import technology.sola.engine.assets.audio.AudioClip;
 import technology.sola.engine.editor.core.components.EditorPanel;
+import technology.sola.engine.platform.javafx.assets.audio.JavaFxAudioClipAssetLoader;
 
 import java.io.File;
 
+@NullMarked
 class AudioPlayerPanel extends EditorPanel {
-  private File audioFile;
+  @Nullable
+  private AudioClip audioClip;
+  private Button playPauseButton;
 
   AudioPlayerPanel(File audioFile) {
-    this.audioFile = audioFile;
+    playPauseButton = new Button("Play");
+    playPauseButton.setDisable(true);
+
+    playPauseButton.setOnAction(event -> {
+      if (audioClip.isPlaying()) {
+        playPauseButton.setText("Play");
+        audioClip.pause();
+      } else {
+        playPauseButton.setText("Pause");
+        audioClip.play();
+      }
+    });
+
+    getChildren().add(buildButtonsUi());
+
+    new JavaFxAudioClipAssetLoader()
+      .getNewAsset(audioFile.getPath(), audioFile.getAbsolutePath())
+      .executeWhenLoaded(audioClip -> {
+        this.audioClip = audioClip;
+
+        audioClip.addFinishListener(AudioClip::stop);
+
+        playPauseButton.setDisable(false);
+      });
   }
+
+  void cleanup() {
+    if (audioClip != null) {
+      audioClip.dispose();
+    }
+  }
+
+  private HBox buildButtonsUi() {
+    HBox container = new HBox();
+
+    container.getChildren().add(playPauseButton);
+
+    return container;
+  }
+
+  // todo loop button
+  // todo stop button
+  // todo volume up button
+  // todo volume down button
+  // todo volume number field
 }
