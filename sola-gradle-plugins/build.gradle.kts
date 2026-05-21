fe84c8bd1dac74c21d2093c6d62a2b4beb8eb8c3
iamdudeman <timothy.solum@gmail.com>
2026-05-21 18:07:36 -0500

    updated gradle to 9.5.0

--- sola-gradle-plugins/build.gradle.kts
+++ sola-gradle-plugins/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   `kotlin-dsl`
   `java-gradle-plugin`
   `maven-publish`
-  id("com.gradle.plugin-publish") version "2.0.0"
+  id("com.gradle.plugin-publish") version "2.1.1"
 }
 
 val props = Properties()
